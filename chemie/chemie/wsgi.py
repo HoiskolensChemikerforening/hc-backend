3739d95ebaa107f2b3dcb4552748e94d93b12837
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-02-20 12:59:28 +0100

    init

--- chemie/chemie/wsgi.py
+++ chemie/chemie/wsgi.py
@@ -4,15 +4,13 @@ WSGI config for chemie project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-from mezzanine.utils.conf import real_project_name
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "%s.settings" % real_project_name("chemie"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chemie.settings")
 
 application = get_wsgi_application()
