1a79628ce486487df6aa26f080ddf668a726f4d1
sindreoy <sindre.bakke.oyen@gmail.com>
2016-02-21 20:17:03 +0100

    Lastet opp sladreboks models, views og urls

--- chemie/shitbox/views.py
+++ chemie/shitbox/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+from .shitbox import Post
+
+def get_name(request):
+    if request.method == 'POST':
+        form = Post(request.POST)
+        if (is_valid(form)):
+            return HttpResponseRedirect('/Takk/')
+    else:
+        form = Post()
