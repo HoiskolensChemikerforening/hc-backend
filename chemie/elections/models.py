9880a115bd9b8d7118b624d7878a253a4c9fa89f
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-02-21 16:27:13 +0100

    Added views for elections

--- chemie/elections/models.py
+++ chemie/elections/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-User = settings.AUTH_USER_MODEL
+from django.contrib.auth.models import User
 
 class Position(models.Model):
     """docstring for Position"""
@@ -20,6 +20,14 @@ class Position(models.Model):
     def deactivate(self):
         self.active = False
 
+
+class Ticket(models.Model):
+    """docstring for Ticket"""
+    secret = models.CharField(max_length = 12)
+    def __init__(self, arg):
+        super(Ticket, self).__init__()
+        self.arg = arg
+
 class Candidate(models.Model):
     """docstring for Candidate"""
     postition = models.ForeignKey(Position)
@@ -34,17 +42,15 @@ class Candidate(models.Model):
         return self.user.name
 
 
+
 class Vote(models.Model):
     """docstring for Vote"""
+    candidate = models.ForeignKey(Candidate)
+    ticket = models.ForeignKey(Ticket)
     time = models.TimeField(auto_now_add=True)
     def __init__(self, arg):
         super(Vote, self).__init__()
         self.arg = arg
 
-class Ticket(models.Model):
-    """docstring for Ticket"""
-    secret = models.CharField(max_length = 12)
-    def __init__(self, arg):
-        super(Ticket, self).__init__()
-        self.arg = arg
+
 # Create your models here.
