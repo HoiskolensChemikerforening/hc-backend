7c99e53b3c9468ca023fefae43d15ebbbf0a24be
hambro <carljohan.hambro@gmail.com>
2016-02-21 14:19:42 +0100

    Updated model for voting

--- chemie/elections/models.py
+++ chemie/elections/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 User = settings.AUTH_USER_MODEL
 
+
+class Candidate(models.Model):
+    """docstring for Candidate"""
+    postition = models.ForeignKey(Position)
+    user = models.ForeignKey(User)
+    votes = models.ForeignKey(Ticket, through = 'Vote')
+
+    def __init__(self, arg):
+        super(Candidate, self).__init__()
+        self.arg = arg
+
+    def __str__(self
+        return self.user.name
+
+class Position(models.Model):
+    """docstring for Position"""
+    name = models.CharField(max_length = 20)
+    seats = models.SmallIntegerField()
+    active = models.BooleanField(verbose_name="Currently voting")
+    def __init__(self, arg):
+        super(Position, self).__init__()
+        self.arg = arg
+
+    def __str__(self):
+        return self.name
+
 class Vote(models.Model):
     """docstring for Vote"""
     def __init__(self, arg):
@@ -12,21 +38,4 @@ class Ticket(models.Model):
     def __init__(self, arg):
         super(Ticket, self).__init__()
         self.arg = arg
-
-class Candidate(models.Model):
-    """docstring for Candidate"""
-    postition = models.ForeignKey(Position, NULL = False)
-    user = models.ForeignKey(user, NULL = False)
-    def __init__(self, arg):
-        super(Candidate, self).__init__()
-        self.arg = arg
-
-class Position(models.Model):
-    """docstring for Position"""
-    def __init__(self, arg):
-        super(Position, self).__init__()
-        self.arg = arg
-
-
-
 # Create your models here.
