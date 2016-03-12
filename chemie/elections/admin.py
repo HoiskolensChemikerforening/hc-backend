4043a697004c9dbb33cd55947cecfc7ad2e89fdc
Martin Meuche <Martin@dhcp-074013.wlan.ntnu.no>
2016-03-12 18:51:19 +0100

    Added tickets to adminpanel and and tested voting.

--- chemie/elections/admin.py
+++ chemie/elections/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Candidate, Position, Election
+from .models import Candidate, Position, Election, Ticket
 
 admin.site.register(Candidate)
 admin.site.register(Position)
 admin.site.register(Election)
+admin.site.register(Ticket)
