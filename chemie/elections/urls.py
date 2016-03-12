cabbbfecc1971f4a9830104e0c54893e79dffe25
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-03-12 18:16:06 +0100

    Fixed typo

--- chemie/elections/urls.py
+++ chemie/elections/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
 from . import views
-from . import forms
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^vote/', views.post_votes )
+    url(r'^$', views.index, name='index'),
+    url(r'^vote/', views.post_votes)
 ]
