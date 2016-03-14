542c5dc12bdc1c9207d1251240311bb5b7916f77
Martin Meuche <Martin@dhcp-10-22-18-94.wlan.ntnu.no>
2016-03-14 18:38:50 +0100

    fixed urls

--- chemie/elections/urls.py
+++ chemie/elections/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^vote/', views.post_votes)
 ]
