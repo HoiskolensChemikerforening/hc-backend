9bedfda505eb389cf5ad0cf3e46c5aa7f9c6259b
hambro <carljohan.hambro@gmail.com>
2016-04-09 14:18:16 +0200

    Updated lockers

--- chemie/lockers/models.py
+++ chemie/lockers/models.py
@@ -1,33 +1,26 @@
 from django.db import models
 from uuid import uuid4 as uuid
+from django.contrib.auth.models import User
 
 
 class Locker(models.Model):
     number = models.PositiveSmallIntegerField()
 
 
-class User(models.Model):
+class LockerUser(models.Model):
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField()
     username = models.CharField(max_length=10)
     created = models.DateField(auto_now=False, auto_now_add=True)
+    internal_user = models.ForeignKey(User)
     ownerships = models.ManyToManyField(Locker, through='Ownership')
 
 
 class Ownership(models.Model):
-    locker = models.ForeignKey(Locker, related_name="ownerships")
-    user = models.ForeignKey(User)
+    locker = models.ForeignKey(Locker)
+    user = models.ForeignKey(LockerUser)
     created = models.DateField(auto_now=False, auto_now_add=True)
     edited = models.DateField(auto_now=True, auto_now_add=False)
-    validation_code = models.UUIDField(default=uuid)
     activation_code = models.UUIDField(default=uuid)
     active = models.BooleanField(default=False)
-
-
-
-
-
-
-
-
