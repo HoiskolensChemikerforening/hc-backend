8a3017342e20588083b120b59522d25e15c06c8f
hambro <carljohan.hambro@gmail.com>
2016-04-09 17:38:44 +0200

    More admin

--- chemie/lockers/admin.py
+++ chemie/lockers/admin.py
@@ -3,15 +3,38 @@ from .models import Locker, LockerUser, Ownership
 
 admin.site.register(Locker)
 
-admin.site.register(Ownership)
 
 class LockerUserAdmin(admin.ModelAdmin):
     list_display = ["username", "internal_user"]
     list_filter = ["created"]
     search_fields = ["username", "internal_user__username"]
     list_display_links = None
+    
     class Meta:
-      model = LockerUser
+        model = LockerUser
+
+
+class OwnershipAdmin(admin.ModelAdmin):
+    list_display = ["get_locker_number", "get_user"]
+
+    def get_user(self, obj):
+        if obj.user.username:
+            return obj.user.username
+        else:
+            return obj.user.internal_user.username
+
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'locker__username'
+
+    def get_locker_number(self, obj):
+        return(obj.locker.number)
+
+    get_locker_number.short_description = "number"
+    get_locker_number.admin_order_field = "Skapnummer"
+
+    class Meta:
+        model = Ownership
 
 
 admin.site.register(LockerUser, LockerUserAdmin)
+admin.site.register(Ownership, OwnershipAdmin)
