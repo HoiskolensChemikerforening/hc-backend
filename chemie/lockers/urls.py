10d620c3f1cf275cbc795639617addff45f9bb5b
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-04-12 15:50:34 +0200

    Added a template for registering a locker. Not nearly finished. It needs forms.py for csrf validation and to save data to the DB. Added 'registrer-skap/skapnummer' to urls and added a view for registering unoccupied lockers.

--- chemie/lockers/urls.py
+++ chemie/lockers/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from . import views
+from . import views, models
 
 urlpatterns = [
     url(r'^$', views.view_lockers, name='index'),
+    url(r'^registrere-skap/(?P<number>[0-9]+)/$', views.register_locker,
+    name = 'register'),
 ]
