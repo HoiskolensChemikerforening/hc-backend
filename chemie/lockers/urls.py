a419393a4667888a7b914936b8bd7b6fe760d5de
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-05-06 00:03:16 +0200

    Added pagination in Lockers

--- chemie/lockers/urls.py
+++ chemie/lockers/urls.py
@@ -4,5 +4,6 @@ from . import views, models
 
 urlpatterns = [
     url(r'^$', views.view_lockers, name='index'),
-    url(r'^registrere-skap/(?P<number>[0-9]+)/$', views.register_locker,name='register'),
+    url(r'^group=(?P<page>[0-9]+)$', views.view_lockers, name='index'),
+    url(r'^registrere-skap/(?P<number>[0-9]+)/$', views.register_locker, name='register'),
 ]
