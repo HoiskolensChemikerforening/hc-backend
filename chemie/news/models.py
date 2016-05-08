197afc3675b4920ae91b931b9ef16b3d963af81e
Martin Meuche <Martin@Martins-MacBook-Air.local>
2016-05-09 00:01:22 +0200

    not fully working

--- chemie/news/models.py
+++ chemie/news/models.py
@@ -1,12 +1,19 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 class NewsPost(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(null=True, blank=True)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    image = models.FileField(null=True, blank=True)
     #author
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(NewsPost, self).save(*args, **kwargs)
--- chemie/news/urls.py
+++ chemie/news/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^$/(?P<slug>[\w-]+)/$', views.singlePost, name='blog_detail')
+
 ]
