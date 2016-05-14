d73de046a36c37fa53fa472f66efb321dff6b8dc
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-05-15 00:27:23 +0200

    Updated yearbook to reflect new user Profile model. Added year in template

--- chemie/yearbook/urls.py
+++ chemie/yearbook/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from . import views, models
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<year>[0-9]+)/$', views.get_images),
+    url(r'^(?P<year>[0-9]+)/$', views.index),
 
 ]
