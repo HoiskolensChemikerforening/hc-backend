381f2e1953ab8d95a87cb81e2848c369830e4a6e
hambro <carljohan.hambro@gmail.com>
2016-05-15 21:51:40 +0200

    Improved layout

--- chemie/static/js/init.js
+++ chemie/static/js/init.js
@@ -5,3 +5,9 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
