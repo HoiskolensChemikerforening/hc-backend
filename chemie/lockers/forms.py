09f3af971a4998ca681515486e4e88fc04021390
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-05-16 23:16:33 +0200

    Improved locker layout

--- chemie/lockers/forms.py
+++ chemie/lockers/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from .models import LockerUser, Locker, Ownership
 from captcha.fields import ReCaptchaField
-
+from material import *
 
 class RegisterExternalLockerUserForm(forms.ModelForm):
     captcha = ReCaptchaField()
-
+    layout = Layout(Row('first_name', 'last_name'),
+                    Row('username'),)
     class Meta:
         model = LockerUser
         fields = [
