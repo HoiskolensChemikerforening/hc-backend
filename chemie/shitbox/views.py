fa2ce1d8569d2d88d9b8818b84af2e51cdad2b26
hambro <carljohan.hambro@gmail.com>
2016-05-19 22:11:28 +0200

    Made shitbox more material

--- chemie/shitbox/views.py
+++ chemie/shitbox/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .forms import Postform
 
-
+@login_required
 def post_votes(request):
     form = Postform(request.POST or None)
     if form.is_valid():
