2eec0aed7be9439107a7c5e9bb8d2abe451cdc22
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-05-20 22:53:30 +0200

    Cleaned up user registrations

--- chemie/static/js/init.js
+++ chemie/static/js/init.js
@@ -7,7 +7,6 @@
 })(jQuery); // end of jQuery name space
 
 
-  $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
+$(document).ready(function() {
+  $('select').material_select();
+});
