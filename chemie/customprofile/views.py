dcd9b21ac913717906c1d6752f772bfed4755e12
Martin Meuche <Martin@Martins-MacBook-Air.local>
2016-05-28 21:22:12 +0200

    attempted loginsystem, not fully functional

--- chemie/customprofile/views.py
+++ chemie/customprofile/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from customprofile.models import Profile, GRADES
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, Http404
-from .forms import RegisterUserForm, RegisterProfileForm
+from .forms import RegisterUserForm, RegisterProfileForm, UserLoginForm
+from django.contrib.auth import authenticate, login
 
 
 def register_user(request):
@@ -29,3 +30,32 @@ def register_user(request):
         "user_profile_form": user_profile_form,
     }
     return render(request, 'userregistration/register.html', context)
+
+def user_login(request):
+    login_form = UserLoginForm(request.POST or None)
+    context = {
+    "login_form": login_form
+    }
+    if login_form.is_valid():
+        username = login_form.username
+        password = login_form.password
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                context = {
+                "message": 'Du er nå inlogget!'
+                }
+                return render(request, 'common/feedback.html', context)
+            else:
+                context = {
+                "message": "Brukeren eksisterer ikke"
+                }
+                return render(request, 'common/feedback.html', context)
+        else:
+            context={
+            "message": "Beklager, enten passord eller bruknavnet er feil."
+            }
+            return render(request, 'common/feedback.html', context)
+
+    return render(request, 'userregistration/login.html', context)
