6ff5c6445b1e113f9bbd0b1dd27901620c582ea7
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-05-30 14:17:08 +0200

    Now able to log in and display username if logged in. If logged in it displays a logout button. Edited register profile url to root/profile/register

--- chemie/customprofile/urls.py
+++ chemie/customprofile/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.register_user),
+    url(r'^register$', views.register_user),
 ]
--- chemie/home/views.py
+++ chemie/home/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 
 def index(request):
-	return render_to_response('home/index.html')
+	return render(request, 'home/index.html')
