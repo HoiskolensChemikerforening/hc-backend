c96e93f43ddc29eb7d67d8459b85849db39a1425
hambro <carljohan.hambro@gmail.com>
2016-05-31 23:58:21 +0200

    Polishing locker layout/design

--- chemie/static/js/init.js
+++ chemie/static/js/init.js
@@ -10,3 +10,13 @@
 $(document).ready(function() {
   $('select').material_select();
 });
+
+  $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      //ready: function() { alert('Ready'); }, // Callback for Modal open
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
