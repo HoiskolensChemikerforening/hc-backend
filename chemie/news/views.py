5932c36154ba41d26c005ad9eff484da1035d9ff
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-06-06 22:26:53 +0200

    Bugfix and PEP8 for News

--- chemie/news/views.py
+++ chemie/news/views.py
@@ -4,25 +4,27 @@ from django.template import RequestContext
 from .models import Article
 from .forms import Newsform
 
+
 def index(request):
     all_articles = Article.objects.all()
-    for post in all_articles:
-        context = {
-            'articles': all_articles
-            }
-    return render(request,'news/detail.html', context)
+    context = {
+        'articles': all_articles
+    }
+    return render(request, 'news/overview.html', context)
+
 
 def display_article(request, slug):
-    article = Article.objects.all()
+    article = Article.objects.get(slug=slug)
     context = {
         'article': article
     }
     return render(request, 'news/single.html', context)
 
+
 def create_article(request):
     post = Newsform(request.POST or None)
     if post.is_valid():
-        news = post.save()
+        post.save()
         context = {
             "title": 'Fullført',
             "message": 'Artikkelen har blitt postet.',
