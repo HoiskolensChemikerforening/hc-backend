da74486f61aab9943f8bafe6d0ac9c2bbc4de39e
hambro <carljohan.hambro@gmail.com>
2016-06-06 23:08:41 +0200

    Fixed typo

--- chemie/news/models.py
+++ chemie/news/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.template.defaultfilters import slugify
-# Create your models here.
+
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(null=True, blank=True)
+    ingress_content = models.TextField()
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     image = models.FileField(null=True, blank=True)
@@ -12,8 +12,3 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Article, self).save(*args, **kwargs)
