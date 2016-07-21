79004f0d293c652c4691d8d32c2e8638c183963c
sindreoy <sindreoy@stud.ntnu.no>
2016-07-21 03:03:30 +0200

    Added functionality to click a specific event for more information

--- chemie/events/views.py
+++ chemie/events/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import RegisterEvent
 from django.contrib.auth.decorators import login_required
+from .models import Event
+from datetime import datetime
 # Create your views here.
 
 @login_required
@@ -11,8 +13,23 @@ def register_event(request):
             instance = form.save(commit=False)
             instance.author = request.user
             instance.save()
-
     context = {
         'form': form,
     }
     return render(request, 'events/register_event.html', context)
+
+def list(request):
+    all_events = Event.objects.filter(date__gt=datetime.now())
+    context = {
+        'events': all_events,
+    }
+    return render(request, "events/list.html", context)
+
+#def index(request):
+#    event = request's ID to specific event
+#    context = {
+#        'event': event
+#    return render(request, "events/index.html", context)
+
+# This view is supposed to view more info about a
+# specific event if you click its title
