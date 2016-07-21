5437286070a29daf731eb0f3b06b5927fcdf0769
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-07-21 03:03:30 +0200

    Added functionality to click a specific event for more information

--- chemie/events/urls.py
+++ chemie/events/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'create', views.register_event),
+    url(r'^$', views.list),
+    url(r'^create', views.register_event),
 ]
