af3b686da979ca7d05c4c12b8d462e04ec608653
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-07-21 03:14:09 +0200

    Made pretty form

--- chemie/events/forms.py
+++ chemie/events/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-
+import material as M
 from .models import Event
 
 class RegisterEvent(forms.ModelForm):
+    layout = M.Layout(M.Row('title'),
+                      M.Row('date'),
+                      M.Row('register_startdate', 'register_deadline','deregister_deadline'),
+                      M.Row('location'),
+                      M.Row('description'),
+                      M.Row('sluts'),
+                      M.Row('price_member','price_not_member','price_companion'),
+                      M.Row('companion','sleepover', 'night_snack','mail_notification'),)
+
+
     class Meta:
         model = Event
         fields = [
