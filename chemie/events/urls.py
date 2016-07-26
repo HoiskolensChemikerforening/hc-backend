aaad339c2f5dcee1355ef53c99222ec52049ea7f
hambro <carljohan.hambro@gmail.com>
2016-07-26 16:07:36 +0200

    Added detail view for events

--- chemie/events/urls.py
+++ chemie/events/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.list),
-    url(r'^create', views.register_event),
+    url(r'^$', views.list_all),
+    url(r'^create', views.create_event),
+    url(r'^(?P<event_id>[0-9]+)$', views.view_event_details, name='detail'),
 ]
