c3fac01ad748a573ebc714e6017443c0ca5f5b9f
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-08-01 12:48:24 +0200

    Improved look and feel of events

--- chemie/static/js/init.js
+++ chemie/static/js/init.js
@@ -21,9 +21,4 @@ $(document).ready(function() {
     }
   );
 
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    formatSubmit: 'yyyy:mm:dd',
-    hiddenName: true
-  });
+
