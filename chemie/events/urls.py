19390d05d923e93ad0ed4d484e33d006233637f7
hambro <carljohan.hambro@gmail.com>
2016-08-03 02:55:38 +0200

    Missing URL regex

--- chemie/events/urls.py
+++ chemie/events/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^create', views.create_event),
     url(r'^(?P<event_id>[0-9]+)', views.view_event_details, name='detail'),
     url(r'^register/(?P<event_id>[0-9]+)', views.register_user, name='register'),
-    url(r'^adminlist', views.view_admin_panel, name='adminlist'),
+    url(r'^adminlist/(?P<event_id>[0-9]+)', views.view_admin_panel, name='adminlist'),
 ]
