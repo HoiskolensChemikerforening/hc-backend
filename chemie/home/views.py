5976f22b6fbf5bc6c96e088f6c9bbc21d9e5749d
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-09-02 16:05:49 +0200

    Add events to frontpage

--- chemie/home/views.py
+++ chemie/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.template import RequestContext
-
+from events.models import Event
+from django.utils import timezone
 
 def index(request):
-	return render(request, 'home/index.html')
+    all_events = Event.objects.filter(date__gt=timezone.now())
+    context = {
+        'events': all_events
+    }
+    return render(request, 'home/index.html', context)
