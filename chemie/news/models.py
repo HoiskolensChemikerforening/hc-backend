b2020cf7796be1e237bd55d4695e4a91b0715c20
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-09-08 23:37:17 +0100

    Created news all over, old one sucked. Not quite done, but reformatting so pushing now.

--- chemie/news/models.py
+++ chemie/news/models.py
@@ -1,14 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
+
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(null=True, blank=True)
-    ingress_content = models.TextField()
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     image = models.FileField(null=True, blank=True)
-    #author
+    author = models.ForeignKey(User)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+
+        super(Article, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('events:detail', kwargs={"event_id": self.id})
+
+
+    #def get_absolute_registration_url(self):
+    #    return reverse('events:register', kwargs={"event_id": self.id})
