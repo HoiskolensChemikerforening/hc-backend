c092f6760fac70f2bd99f67e4ed8a6d0c3072300
sindreoy <sindreoy@stud.ntnu.no>
2016-09-10 19:20:42 +0200

    Completely redid news app.

--- chemie/news/models.py
+++ chemie/news/models.py
@@ -1,30 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.template.defaultfilters import slugify
+from django.utils.text import slugify
+from sorl.thumbnail import ImageField
+from django.db.models.signals import pre_save
 
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(null=True, blank=True)
+    slug = models.SlugField()
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    image = models.FileField(null=True, blank=True)
+    image = ImageField(upload_to='news', verbose_name="Bilde")
     author = models.ForeignKey(User)
 
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.slug = slugify(self.title)
-
-        super(Article, self).save(*args, **kwargs)
-
     def get_absolute_url(self):
-        return reverse('events:detail', kwargs={"event_id": self.id})
+        return reverse('news:detail', kwargs={"article_id": self.id, 'slug':self.slug})
 
 
-    #def get_absolute_registration_url(self):
-    #    return reverse('events:register', kwargs={"event_id": self.id})
+def pre_save_article_receiver(sender, instance, *args, **kwargs):
+    slug = slugify(instance.title)
+    instance.slug = slug
+
+
+pre_save.connect(pre_save_article_receiver, sender=Article)
--- chemie/news/views.py
+++ chemie/news/views.py
@@ -1,28 +1,33 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Article
 from .forms import NewsPost
 
 def create_post(request):
-    post = NewsPost(request.POST or None)
+    post = NewsPost(request.POST or None, request.FILES or None)
     if request.POST:
-        instance = post.save(commit=False)
-        instance.author = request.user
-        instance.save()
-
+        if post.is_valid():
+            instance = post.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return HttpResponseRedirect(reverse('news:index'))
     context = {
         'post': post
     }
     return render(request, 'news/create_post.html', context)
 
-def news_details(request, article_id):
-    article = Article.objects.get(year__gt=datetime.now())
+def news_details(request, article_id, slug):
+    article = get_object_or_404(Article, id=article_id)
+    context = {
+        'article': article
+    }
+    return render(request, 'news/detail.html', context)
 
 def list_all(request):
     all_posts = Article.objects.all()
     context = {
         'posts': all_posts,
     }
-    return render(request, 'news/list_all', context)
+    return render(request, 'news/list.html', context)
