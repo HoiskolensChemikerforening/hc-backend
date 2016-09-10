77806d959129d79afa1110451eadff1be0f829fa
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-09-10 19:19:12 +0200

    Added news to frontpage and removed events from frontpage.

--- chemie/home/views.py
+++ chemie/home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from events.models import Event
+from news.models import Article
 from django.utils import timezone
 
 def index(request):
     all_events = Event.objects.filter(date__gt=timezone.now())
+    all_posts = Article.objects.all()
     context = {
-        'events': all_events
+        'events': all_events,
+        'posts': all_posts,
     }
     return render(request, 'home/index.html', context)
