cd63fd36d90bb81d056fe9c01eea57859b94113b
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-09-10 19:20:42 +0200

    Completely redid news app.

--- chemie/news/urls.py
+++ chemie/news/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.list_all),
-    url(r'^detail/(?P<article_id>[0-9]+)/[\w0-9/]+', views.news_details),
-    url(r'^create', views.create_post),
+    url(r'^$', views.list_all, name = 'index'),
+    url(r'^detail/(?P<article_id>[0-9]+)/(?P<slug>[\w0-9/]+)', views.news_details, name='detail'),
+    url(r'^create', views.create_post, name = 'create')
 ]
