4e68efc2c142d3d10eb87eca317ebd2aec8c4002
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-09-18 17:57:19 +0200

    Add forgotten app and PEP8

--- chemie/chemie/views.py
+++ chemie/chemie/views.py
@@ -1,11 +1,13 @@
 from random import choice
-from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from .models import pictures_for_404
 
 
 def show_404(request):
     img_404 = pictures_for_404.objects.all()
-    rand_img = choice(img_404)
+    try:
+        random_image = choice(img_404)
+    except IndexError:
+        random_image = None
 
-    return render(request, 'chemie/404.html', context={'bilde':rand_img}, status=404)
+    return render(request, 'chemie/404.html', context={'bilde': random_image}, status=404)
