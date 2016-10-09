48aa6e3b520f0feb17eb6f39fd9c857cb99cb0c6
Martin Meuche <martin_meuche@hotmail.com>
2016-10-09 17:58:02 +0200

    Created functionality to manage members

--- chemie/customprofile/urls.py
+++ chemie/customprofile/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^changepassword$', views.change_password, name='changepassword'),
     url(r'^forgotpassword', views.forgot_password, name='forgotpassword'),
     url(r'^aktiver/(?P<code>[a-z0-9]{32})/?', views.activate_password, name='activate'),
+    url(r'^medlemmer/endre/(?P<id>[0-9]+)', views.change_membership_status, name='membership'),
+    url(r'^medlemmer', views.manage_memberships, name='memberships'),
 ]
