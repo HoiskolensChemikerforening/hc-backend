dbe18222196bb24ca4ef3266f5864f1ab2ca91a0
Eirikhn <eirikhnygaard@gmail.com>
2016-10-09 16:39:53 +0200

    Add search field on yearbook

--- chemie/yearbook/views.py
+++ chemie/yearbook/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from customprofile.models import Profile, GRADES
-
+from customprofile.models import Profile, GRADES, ProfileManager
+from django.http import HttpResponseRedirect
+from .forms import NameSearchForm
 
 def index(request, year=1):
     year = int(year)
@@ -14,5 +15,22 @@ def index(request, year=1):
     context = {
         'profiles': profiles,
         'grades': GRADES,
+        'search_form': NameSearchForm(None),
     }
     return render(request, 'yearbook/get_images.html', context)
+
+
+def search_user(request):
+    if request.method == 'POST':
+        form = NameSearchForm(request.POST)
+        if form.is_valid():
+            name_terms = form.cleaned_data.get('search_field').split(' ')
+            profiles = Profile.objects.search_name(name_terms)
+            context = {
+                'profiles': profiles,
+                'search_form': NameSearchForm(None),
+            }
+            return render(request, 'yearbook/get_images.html', context)
+        else:
+            return HttpResponseRedirect('/klassekatalog/')
+    return HttpResponseRedirect('/klassekatalog/')
