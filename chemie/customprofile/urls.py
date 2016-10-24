51f17522b173587ba44bf68cace908541a9a0dea
sindreoy <sindreoy@stud.ntnu.no>
2016-10-24 21:58:47 +0200

    Now able to add a membership to a user on host/profile/medlemmer. Also optimized some other code

--- chemie/customprofile/urls.py
+++ chemie/customprofile/urls.py
@@ -5,9 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^register$', views.register_user, name='register'),
     url(r'^edit$', views.edit_profile, name='edit'),
-    url(r'^changepassword$', views.change_password, name='changepassword'),
     url(r'^forgotpassword', views.forgot_password, name='forgotpassword'),
     url(r'^aktiver/(?P<code>[a-z0-9]{32})/?', views.activate_password, name='activate'),
-    url(r'^medlemmer/endre/(?P<id>[0-9]+)', views.change_membership_status, name='membership'),
-    url(r'^medlemmer', views.manage_memberships, name='memberships'),
+    url(r'^medlemmer/endre/(?P<profile_id>[0-9]+)', views.change_membership_status, name='membership'),
+    url(r'^medlemmer', views.view_memberships, name='memberships'),
 ]
