70ea43d1b5b2b3e5d762fba1b34cc3c1b817ae31
hambro <carljohan.hambro@gmail.com>
2016-10-26 00:53:24 +0200

    Add cache for sponsors

--- chemie/chemie/templatetags/chemie_tags.py
+++ chemie/chemie/templatetags/chemie_tags.py
@@ -1,8 +1,11 @@
 from django import template
 from chemie.models import Sponsor
 from django.utils import timezone
+from django.views.decorators.cache import cache_page
+
 register = template.Library()
 
+@cache_page(60 * 60)
 @register.inclusion_tag('sponsor/sponsor_list.html')
 def show_sponsors():
     sponsors = Sponsor.objects.filter(end_date__gte=timezone.now())
