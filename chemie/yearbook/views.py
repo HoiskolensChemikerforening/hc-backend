8007e267a5525386fe1e14530f1f3166ef1e93ab
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-11-02 01:42:21 +0100

    Updated search function in yearbook. Works a little different from before.

--- chemie/yearbook/views.py
+++ chemie/yearbook/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from customprofile.models import Profile, GRADES, ProfileManager
-from django.http import HttpResponseRedirect
+from customprofile.models import Profile, GRADES
 from .forms import NameSearchForm
+from django.db.models import Q
+from django.contrib.auth.models import User
+
 
 def index(request, year=1):
     year = int(year)
@@ -10,27 +12,25 @@ def index(request, year=1):
             year = GRADES.FIFTH.value
         else:
             year = 1
-
-    profiles = Profile.objects.filter(grade=year)
+    form = NameSearchForm(request.POST or None)
+    profiles = Profile.objects.none()
+    if request.method == 'POST':
+        if form.is_valid():
+            search_field = form.cleaned_data.get('search_field')
+            users = find_user_by_name(search_field)
+            profiles = Profile.objects.filter(user__in=users)
+    else:
+        profiles = Profile.objects.filter(grade=year)
     context = {
         'profiles': profiles,
         'grades': GRADES,
-        'search_form': NameSearchForm(None),
+        'search_form': form,
     }
     return render(request, 'yearbook/get_images.html', context)
 
 
-def search_user(request):
-    if request.method == 'POST':
-        form = NameSearchForm(request.POST)
-        if form.is_valid():
-            name_terms = form.cleaned_data.get('search_field').split(' ')
-            profiles = Profile.objects.search_name(name_terms)
-            context = {
-                'profiles': profiles,
-                'search_form': NameSearchForm(None),
-            }
-            return render(request, 'yearbook/get_images.html', context)
-        else:
-            return HttpResponseRedirect('/klassekatalog/')
-    return HttpResponseRedirect('/klassekatalog/')
+def find_user_by_name(query_name):
+    qs = User.objects.all()
+    for term in query_name.split():
+        qs = qs.filter( Q(first_name__icontains=term) | Q(last_name__icontains=term))
+    return qs
