62908e208ba387b0d9e5fc04baa5fb9f9235ead2
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2016-11-06 23:56:53 +0100

    Search function in yearbook looks better now. Also reformatted some code

--- chemie/news/views.py
+++ chemie/news/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Article
 from .forms import NewsPost
 
+
 def create_post(request):
     post = NewsPost(request.POST or None, request.FILES or None)
     if request.POST:
@@ -18,12 +19,14 @@ def create_post(request):
     }
     return render(request, 'news/create_post.html', context)
 
+
 def news_details(request, article_id, slug):
     article = get_object_or_404(Article, id=article_id)
     context = {
         'article': article
     }
     return render(request, 'news/detail.html', context)
+
 
 def list_all(request):
     all_posts = Article.objects.all()
--- chemie/yearbook/forms.py
+++ chemie/yearbook/forms.py
@@ -1,9 +1,4 @@
 from django import forms
-from django.contrib.auth.models import User
-
-
-#class NameSearchForm(forms.Form):
-#    search_field = forms.CharField(label=' ', max_length=100)
 
 
 class NameSearchForm(forms.Form):
