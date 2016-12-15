9d339c03d9377ca7cfcf0bf0909c4499007b7654
Carl Johan Hambro <carljohan.hambro@gmail.com>
2016-12-15 15:12:13 +0100

    Correct translation

--- chemie/events/forms.py
+++ chemie/events/forms.py
@@ -4,29 +4,29 @@ from .models import Event, Registration
 from datetime import datetime
 from django.core.validators import ValidationError
 
+
 class RegisterEventForm(forms.ModelForm):
     layout = M.Layout(M.Row('title'),
-                      M.Row(M.Column('event_date','event_time', span_columns=1),
-                            M.Column('registration_start_date','registration_start_time', span_columns=1),
-                            M.Column('register_deadline_date','register_deadline_time', span_columns=1),
-                            M.Column('deregister_deadline_date','deregister_deadline_time', span_columns=1)),
+                      M.Row(M.Column('event_date', 'event_time', span_columns=1),
+                            M.Column('registration_start_date', 'registration_start_time', span_columns=1),
+                            M.Column('register_deadline_date', 'register_deadline_time', span_columns=1),
+                            M.Column('deregister_deadline_date', 'deregister_deadline_time', span_columns=1)),
                       M.Row('location'),
                       M.Row('description'),
                       M.Row('image'),
                       M.Row('payment_information'),
                       M.Row('sluts'),
-                      M.Row('price_member','price_not_member','price_companion'),
-                      M.Row('companion','sleepover', 'night_snack','mail_notification'),)
+                      M.Row('price_member', 'price_not_member', 'price_companion'),
+                      M.Row('companion', 'sleepover', 'night_snack', 'mail_notification'), )
 
-    event_date = forms.DateField(required=True)
-    event_time = forms.TimeField(required=True, initial='12:00')
-    registration_start_date = forms.DateField(required=True)
-    registration_start_time = forms.TimeField(required=True, initial='12:00')
-    register_deadline_date = forms.DateField(required=True)
-    register_deadline_time = forms.TimeField(required=True, initial='12:00')
-    deregister_deadline_date = forms.DateField(required=True)
-    deregister_deadline_time = forms.TimeField(required=True, initial='12:00')
-
+    event_date = forms.DateField(required=True, label='Arrangemantsdato')
+    event_time = forms.TimeField(required=True, initial='12:00', label='Arrangemantstid')
+    registration_start_date = forms.DateField(required=True, label='Åpningsdato')
+    registration_start_time = forms.TimeField(required=True, initial='12:00', label='Åpningstid')
+    register_deadline_date = forms.DateField(required=True, label='Påmeldingsfristdato')
+    register_deadline_time = forms.TimeField(required=True, initial='12:00', label='Påmeldingsfristtidspunkt')
+    deregister_deadline_date = forms.DateField(required=True, label='Avmeldingsdato')
+    deregister_deadline_time = forms.TimeField(required=True, initial='12:00', label='Avmeldingstidspunkt')
 
     def clean(self):
         super(RegisterEventForm, self).clean()
@@ -34,24 +34,32 @@ class RegisterEventForm(forms.ModelForm):
             return
 
         event_date = datetime.combine(self.cleaned_data.get('event_date'), self.cleaned_data.get('event_time'))
-        registration_date = datetime.combine(self.cleaned_data.get('registration_start_date'), self.cleaned_data.get('registration_start_time'))
-        registration_deadline = datetime.combine(self.cleaned_data.get('register_deadline_date'), self.cleaned_data.get('register_deadline_time'))
-        deregister_deadline = datetime.combine(self.cleaned_data.get('deregister_deadline_date'), self.cleaned_data.get('deregister_deadline_time'))
+        registration_date = datetime.combine(self.cleaned_data.get('registration_start_date'),
+                                             self.cleaned_data.get('registration_start_time'))
+        registration_deadline = datetime.combine(self.cleaned_data.get('register_deadline_date'),
+                                                 self.cleaned_data.get('register_deadline_time'))
+        deregister_deadline = datetime.combine(self.cleaned_data.get('deregister_deadline_date'),
+                                               self.cleaned_data.get('deregister_deadline_time'))
 
         if event_date < registration_date:
-            self.add_error(None, ValidationError({'registration_start_date':["Påmeldingen må være før arrangementet begynner"]}))
+            self.add_error(None, ValidationError(
+                {'registration_start_date': ["Påmeldingen må være før arrangementet begynner"]}))
 
         if event_date < registration_deadline:
-            self.add_error(None, ValidationError({'register_deadline_date':["Påmeldingsfristen må være før arrangementet begynner"]}))
+            self.add_error(None, ValidationError(
+                {'register_deadline_date': ["Påmeldingsfristen må være før arrangementet begynner"]}))
 
         if event_date < deregister_deadline:
-            self.add_error(None, ValidationError({'deregister_deadline_date':["Avmeldingsfristen må være før arrangementet begynner"]}))
+            self.add_error(None, ValidationError(
+                {'deregister_deadline_date': ["Avmeldingsfristen må være før arrangementet begynner"]}))
 
         if registration_deadline < registration_date:
-            self.add_error(None, ValidationError({'register_deadline_date':["Påmeldingsfristen må være etter påmeldingen begynner"]}))
+            self.add_error(None, ValidationError(
+                {'register_deadline_date': ["Påmeldingsfristen må være etter påmeldingen begynner"]}))
 
         if deregister_deadline < registration_date:
-            self.add_error(None, ValidationError({'deregister_deadline_date':["Avmeldingsfristen må være etter påmeldingen begynner"]}))
+            self.add_error(None, ValidationError(
+                {'deregister_deadline_date': ["Avmeldingsfristen må være etter påmeldingen begynner"]}))
 
         self.cleaned_data['date'] = event_date
         self.cleaned_data['register_startdate'] = registration_date
@@ -82,7 +90,6 @@ class RegisterEventForm(forms.ModelForm):
 
 
 class RegisterUserForm(forms.ModelForm):
-
     class Meta:
         model = Registration
 
