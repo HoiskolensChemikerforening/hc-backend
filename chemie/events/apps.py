4dce5c1fd5d252f1fb17fc0088410b90454a0c39
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-01-09 23:57:04 +0100

    Events will move users from the waiting list to confirmed attendees when the number of slots are increased

--- chemie/events/apps.py
+++ chemie/events/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class EventsConfig(AppConfig):
     name = 'events'
+    verbose_name = 'Arrangementer - administrativt'
+
+    def ready(self):
+        import events.signals
