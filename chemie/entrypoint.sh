fa98812696ca12132bc3cd9c7a51196fa0bd5dec
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-02-06 22:06:21 +0100

    Switched to UWSGI

--- chemie/entrypoint.sh
+++ chemie/entrypoint.sh
@@ -7,4 +7,6 @@ if [ "$DEBUG" == "False" ]
 then
   python manage.py collectstatic --noinput
 fi
-exec gunicorn chemie.wsgi:application --log-file=/gunicorn-logfiles/error.log --access-logfile=/gunicorn-logfiles/access.log --log-level INFO -w 2 -b :8000
+exec uwsgi --chdir=./ --module=chemie.wsgi:application --env DJANGO_SETTINGS_MODULE=chemie.settings \
+--master --pidfile=/tmp/project-master.pid     --http=0.0.0.0:8000 --processes=5 \
+--harakiri=20 --max-requests=5000 --vacuum
