c41157afedcc5c29320486881d13101deb3600df
cherrybeans <sigridkvamme@gmail.com>
2017-02-24 02:49:12 +0100

    Made a detail view for yearbook. Added a black_avatar file to static.

--- chemie/chemie/static/js/init.js
+++ chemie/chemie/static/js/init.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
       in_duration: 300, // Transition in duration
       out_duration: 200, // Transition out duration
       //ready: function() { alert('Ready'); }, // Callback for Modal open
-      complete: function() { alert('Closed'); } // Callback for Modal close
+      //complete: function() { alert('Closed'); } // Callback for Modal close
     }
   );
 
