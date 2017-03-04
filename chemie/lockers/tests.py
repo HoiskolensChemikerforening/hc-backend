c192a16e440883d16fbca640dd4de9fe89da85f9
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-03-04 01:51:45 +0100

    Add unit tests for lockers

--- chemie/lockers/tests.py
+++ chemie/lockers/tests.py
@@ -1,8 +1,64 @@
 from django.test import TestCase
-from lockers.models import *
+from lockers.models import LockerUser, Locker, LockerConfirmation, Ownership
+from lockers.models import LOCKER_COUNT
 
-def LockerUserTestCase(TestCase):
+
+def create_user_with_locker(count):
+    user = LockerUser.objects.create(first_name="Glenn",
+                                     last_name="Gregor",
+                                     username='glenny')
+    for i in range(count):
+        locker = Locker.objects.create(number=i)
+        ownership = Ownership.objects.create(locker=locker, user=user)
+        token = LockerConfirmation.objects.create(ownership=ownership)
+        token.activate()
+
+
+class LockerUserLimitTest(TestCase):
     def setUp(self):
-        LockerUser.objects.create(first_name="Jöns Jacob",
-                                  last_name="Berzelius",
-                                  username='jzelius')
+        create_user_with_locker(LOCKER_COUNT-1)
+
+    def test_not_reached_limit(self):
+        ownership = Ownership.objects.filter(user__username='glenny')[0]
+        self.assertEqual(ownership.reached_limit(), False)
+
+    def test_reached_limit(self):
+        ownership = Ownership.objects.filter(user__username='glenny')[0]
+        user = ownership.user
+
+        locker = Locker.objects.create(number=LOCKER_COUNT)
+        ownership = Ownership.objects.create(locker=locker, user=user)
+        token = LockerConfirmation.objects.create(ownership=ownership)
+        token.activate()
+
+        self.assertEqual(ownership.reached_limit(), True)
+
+class ActivationTokenTest(TestCase):
+    def setUp(self):
+        user = LockerUser.objects.create(first_name="Glenn",
+                                         last_name="Gregor",
+                                         username='glenny')
+        locker = Locker.objects.create(number=1)
+        ownership = Ownership.objects.create(locker=locker, user=user)
+        token = LockerConfirmation.objects.create(ownership=ownership)
+
+    def test_locker_inactive(self):
+        locker = Locker.objects.get(number=1)
+        ownership = Ownership.objects.get(locker=locker)
+        self.assertEqual(locker.is_free(), True)
+        self.assertEqual(ownership.is_active, False)
+        self.assertEqual(ownership.is_confirmed, False)
+
+    def test_locker_taken(self):
+        locker = Locker.objects.get(number=1)
+        ownership = Ownership.objects.get(locker=locker)
+        token = LockerConfirmation.objects.get(ownership=ownership)
+        token.activate()
+
+        ownership = Ownership.objects.get(locker=locker)
+        locker = Locker.objects.get(number=1)
+
+        self.assertEqual(locker.is_free(), False)
+        self.assertEqual(ownership.is_active, True)
+        self.assertEqual(ownership.is_confirmed, True)
+        self.assertEqual(token.pk, None)
