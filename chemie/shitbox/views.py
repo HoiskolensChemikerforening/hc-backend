63d313f303897079502ecd4f2336a0e58b4506bc
hambro <carljohan.hambro@gmail.com>
2017-03-05 00:39:57 +0100

    Add overview for 'overheard quotes' submissions

--- chemie/shitbox/views.py
+++ chemie/shitbox/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from .forms import Postform
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from .forms import Postform
+from .models import Submission
 
 @login_required
 def post_votes(request):
@@ -22,3 +25,23 @@ def post_votes(request):
     }
 
     return render(request, "shitbox/post_form.html", context)
+
+
+@login_required
+@permission_required('shitbox.submission.can_view')
+def submissions_overview(request, page=1):
+    all_submissions = Submission.objects.all().order_by('-date')
+    paginator = Paginator(all_submissions, 20)
+
+    try:
+        submissions = paginator.page(page)
+    except PageNotAnInteger:
+        submissions = paginator.page(1)
+    except EmptyPage:
+        submissions = paginator.page(paginator.num_pages)
+
+    context = {
+        'submissions': submissions
+    }
+
+    return render(request, 'shitbox/list_submissions.html', context=context)
