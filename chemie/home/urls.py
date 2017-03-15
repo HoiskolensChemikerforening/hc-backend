c2a77664badea48842a980dc832b52b59b2af1bf
cherrybeans <sigridkvamme@gmail.com>
2017-03-15 19:27:57 +0100

    Added form for applying for funds

--- chemie/home/urls.py
+++ chemie/home/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-
-from .views import index, contact, calendar
+from .views import index, contact, calendar, post_funds_form
 
 urlpatterns = [
     url(r'^$', index, name="home"),
     url(r'^kontakt/', contact, name="kontakt"),
-    url(r'^kalender/', calendar, name="calendar")
+    url(r'^kalender/', calendar, name="calendar"),
+    url(r'^funds', post_funds_form, name="fundsapplication"),
 ]
