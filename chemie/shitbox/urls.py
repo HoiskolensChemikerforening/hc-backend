0c1e8c2f714c8eebb5abb981ee10f1ed6ffd51a5
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-03-15 20:23:52 +0100

    Fix minor bugs with shitbox

--- chemie/shitbox/urls.py
+++ chemie/shitbox/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from .views import submissions_overview, post_votes
 
 urlpatterns = [
-    url(r'list/(?P<page>[0-9]+)$', submissions_overview, name='list'),
+    url(r'list/$', submissions_overview, name='list'),
+    url(r'list/(?P<page>[0-9]*)$', submissions_overview, name='list-page'),
     url(r'^$', post_votes, name="index"),
 ]
