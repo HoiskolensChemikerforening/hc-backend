17c29b39a007d3196c376a323fba3006f40b4cfb
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2017-03-25 15:44:40 +0100

    Created tests for creating an event,  registering users beyond max slots, seeing that they are confirmed or waiting depending on whether there are enough slots, bumping from wait list, confirming registrations and sending event mail. Also removed the email notification field from models.

--- chemie/events/tests.py
+++ chemie/events/tests.py
@@ -1,3 +1,54 @@
+import os
+
+from django.contrib.auth.models import User
+from django.core.files import File
 from django.test import TestCase
+from django.utils import timezone
+
+from chemie import settings
+from events.models import Event, EventRegistration
+
 
 # Create your tests here.
+
+
+class TestEventAndRegistration(TestCase):
+    # Fetch all templates for sending mail
+    fixtures = ['fixtures/email-templates.json']
+
+    def setUp(self):
+        user = User.objects.create(username='glenny')
+        date = timezone.now() + timezone.timedelta(days=5)
+        start_date = timezone.now()
+        reg_deadline = timezone.now() + timezone.timedelta(days=1)
+        dereg_deadline = timezone.now() + timezone.timedelta(days=2)
+        image_path = os.path.join(settings.BASE_DIR, 'media/events/blank_person.png')
+        image = File(image_path)
+        Event.objects.create(title='Indok er helt ok',
+                             author=user,
+                             date=date,
+                             register_startdate=start_date,
+                             register_deadline=reg_deadline,
+                             deregister_deadline=dereg_deadline,
+                             location='Samfundet',
+                             description='Litta fest',
+                             image=image,
+                             sluts=40,
+                             payment_information='Alle maa benytte Vipps til aa betale',
+                             )
+
+    def test_event_registration_exceeding_max_slots(self):
+        event = Event.objects.get(title='Indok er helt ok')
+        for i in range(event.sluts + 5):
+            user = User.objects.create(username=i,
+                                       email='test@mail.com',
+                                       )
+            registration = EventRegistration(event=event,
+                                             user=user,
+                                             )
+            registration.save()
+            # Bumping all users on wait list by
+            # calling overridden save function
+            event.save()
+        self.assertEqual(event.waiting_users(), 5)
+        self.assertEqual(event.registered_users(), event.sluts)
