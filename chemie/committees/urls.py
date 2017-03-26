f36770aee9e96e3dd97030769d36adf3959415d9
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-03-26 23:33:37 +0200

    Refactored URLs for committees

--- chemie/committees/urls.py
+++ chemie/committees/urls.py
@@ -4,14 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='list_all'),
-    url(r'^edit', views.edit, name='edit'),
-    url(
-        r'^user-autocomplete/',
+    url(r'^edit', views.edit, name='edit_memberships'),
+    url(r'^user-autocomplete/',
         views.UserAutocomplete.as_view(),
         name='user-autocomplete',
-    ),
-    url(r'^(?P<slug>[\w-]+)/', include([
-        url(r'^$',  views.view_committee, name='view'),
-        url(r'^edit/', views.edit_description, name='edit_desc'),
-    ])),
+        ),
+    url(r'^(?P<slug>[\w-]+)/$', views.view_committee, name='view'),
+    url(r'^(?P<slug>[\w-]+)/edit/', views.edit_description, name='edit_description'),
 ]
