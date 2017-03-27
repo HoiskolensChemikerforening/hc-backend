71f3494b22aca4ea3c3a66758b58e3e28b6db4ac
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-03-28 00:28:21 +0200

    Updated email templates and squashed a bug

--- chemie/picturecarousel/forms.py
+++ chemie/picturecarousel/forms.py
@@ -1,11 +1,10 @@
 from django import forms
-from django.contrib.auth.models import User
 from .models import Contribution
 import material as M
 
 
 class Pictureform(forms.ModelForm):
-    layout = M.Layout(M.Row('file'))
+    layout = M.Layout(M.Row('image'))
 
     class Meta:
         model = Contribution
