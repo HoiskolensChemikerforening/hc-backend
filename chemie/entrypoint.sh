3ac1b5edb2de7279978446a32e6d43007cc8066a
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-04-10 01:05:37 +0200

    Deamonized uwsgi cron properly

--- chemie/entrypoint.sh
+++ chemie/entrypoint.sh
@@ -8,12 +8,12 @@ then
   python manage.py collectstatic --noinput
 fi
 
+# Cronmanager uwsgi
+exec uwsgi -d /dev/null --chdir=./ --module=chemie.cron_wsgi:application --env DJANGO_SETTINGS_MODULE=chemie.settings \
+--master --pidfile=/tmp/project-master-cron.pid --http=0.0.0.0:8001 --processes=5 \
+--harakiri=20 --max-requests=5000 --vacuum
+
 # Django main uwsgi
 exec uwsgi --chdir=./ --module=chemie.wsgi:application --env DJANGO_SETTINGS_MODULE=chemie.settings \
 --master --pidfile=/tmp/project-master.pid     --http=0.0.0.0:8000 --processes=5 \
---harakiri=20 --max-requests=5000 --vacuum
-
-# Cronmanager uwsgi
-uwsgi --chdir=./ --module=chemie.cron_wsgi:application --env DJANGO_SETTINGS_MODULE=chemie.settings \
---master --pidfile=/tmp/project-master-cron.pid --http=0.0.0.0:8001 --processes=5 \
 --harakiri=20 --max-requests=5000 --vacuum
