47214540606b536639129ebefa71512463dfab6d
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-04-09 16:58:00 +0200

    Add filter, ordering, search by user and snazzed up the table for EventRegistration in admin panel.

--- chemie/events/apps.py
+++ chemie/events/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class EventsConfig(AppConfig):
     name = 'events'
-    verbose_name = 'Arrangementer - administrativt'
+    verbose_name = 'Arrangementer'
 
     def ready(self):
         import events.signals
