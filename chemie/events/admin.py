eb2f194c2976a7b9a57bc4b55be68479f26918f7
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-04-09 16:58:00 +0200

    Add filter, ordering, search by user and snazzed up the table for EventRegistration in admin panel.

--- chemie/events/admin.py
+++ chemie/events/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Event, EventRegistration, RegistrationMessage#, CompanyEvent
+from django.contrib.admin.filters import AllValuesFieldListFilter, RelatedFieldListFilter, ChoicesFieldListFilter
+
+
+class DropdownFilter(RelatedFieldListFilter):
+    template = 'admin/dropdown_filter.html'
 
 
 @admin.register(EventRegistration)
 class RegistrationAdmin(admin.ModelAdmin):
-    list_filter = ('status',)
+    list_filter = ('status', 'payment_status', ('event', DropdownFilter))
+    ordering = ('-created',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+    list_display = ('event', 'user', 'created', 'edited', 'payment_status', 'companion')
+
 
 admin.site.register(RegistrationMessage)
 admin.site.register(Event)
