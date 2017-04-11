880492e7a562c13135d7cfaaed3d5c2cc708b472
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-04-11 15:26:36 +0200

    Update backup job

---
+++ backupjob/run_backup.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+# Daily/Weekly/Monthly/Manual backup
+# REMOTE_HOST
+# REMOTE_SSH_USER 
+# REMOTE_BACKUP_DIR 
+BACKUP_TYPE="$1"
+
+case $BACKUP_TYPE in
+daily)
+    echo "Daily"
+   BACKUP_DIR="daily"
+   TIME_PREFIX=$(date +%A)
+    ;;
+weekly)
+    echo "Weekly"
+   BACKUP_DIR="weekly"
+   TIME_PREFIX=$((($(date +%-d)-1)/7+1))
+    ;;
+monthly)
+    echo "Monthly"
+   BACKUP_DIR="monthly"
+   TIME_PREFIX=$(date +%m)
+    ;;
+*)
+    echo -n "Now"
+   BACKUP_DIR="manual"
+   TIME_PREFIX=$(date +%Y-%m-%d_time_%H%M)
+    ;;
+esac
+
+cd /code/
+if [ "$BACKUP" == "True" ]
+then
+    NOW=$(date +\%a)
+    mkdir /tmp/backup/$NOW -p
+    cd /tmp/backup
+    /usr/bin/pg_dump -U $PGUSER -Fc $POSTGRES_DB > $NOW/backup.gz 2>> /var/log/syslog
+    DBFILE=$REMOTE_BACKUP_DIR/database/$BACKUP_DIR/$TIME_PREFIX.gz
+    MD5=($(md5sum $NOW/backup.gz))
+    scp -r -o StrictHostKeyChecking=no -i /ssh/key $NOW/backup.gz $REMOTE_SSH_USER@$REMOTE_HOST:$DBFILE
+    MD5_SERVER=$(ssh $REMOTE_SSH_USER@$REMOTE_HOST -i /ssh/key -o StrictHostKeyChecking=no DBFILE=$DBFILE 'md5=($(md5sum $DBFILE)); echo $md5')
+    rm -r $NOW
+    REMOTE_PATH=$REMOTE_BACKUP_DIR/media/$BACKUP_DIR/$TIME_PREFIX
+    rsync -vzrltoD -e "ssh -i /ssh/key" --rsync-path="mkdir -p $REMOTE_PATH && rsync" --delete /code/media/ $REMOTE_SSH_USER@$REMOTE_HOST:$REMOTE_PATH
+
+    if [ "$MD5" == "$MD5_SERVER" ]
+    then
+        echo "$(date +'%m.%d.%Y %H:%M') $BACKUP_DIR backup succeded" >> /var/log/backup.log
+    else
+        echo "$(date +'%m.%d.%Y %H:%M') $BACKUP_DIR backup FAILED: MD5 MISMATCH" >> /var/log/backup.log
+    fi
+else
+    echo "$(date +'%m.%d.%Y') Backup disabled" >> /var/log/backup.log
+fi
