e285d0a7252b02263425d4e4a4bd0430e079f6c3
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-08-07 22:41:56 +0200

    Add restriction in backend on max_members limit

--- chemie/committees/forms.py
+++ chemie/committees/forms.py
@@ -1,7 +1,8 @@
 from dal import autocomplete
 from django import forms
+from django.core.validators import ValidationError
 
-from .models import Committee
+from .models import Committee, Position
 
 
 class EditDescription(forms.ModelForm):
@@ -12,3 +13,20 @@ class EditDescription(forms.ModelForm):
             'one_liner',
             'image',
         ]
+
+
+class EditPositionForm(forms.ModelForm):
+    class Meta:
+        model = Position
+        fields = ('users',)
+        widgets = {
+            'users': autocomplete.ModelSelect2Multiple(
+                url='verv:user-autocomplete',
+                attrs={"data-maximum-selection-length": 1, })
+        }
+
+    def clean(self):
+        super(EditPositionForm, self).clean()
+        maximum = self.instance.max_members
+        if self.cleaned_data.get('users').count() > maximum:
+            self.add_error(None, ValidationError({'users': ["Stillingen har maksimalt {} personer".format(maximum)]}))
