f5a8c3e02899b5cd3ac963bd935fa61d80fee3fd
cherrybean <uSCOS1VZDxRoE4H6PKt7>
2017-08-26 15:35:58 +0200

    Shortened paginators in shitbox.

--- chemie/shitbox/views.py
+++ chemie/shitbox/views.py
@@ -29,8 +29,9 @@ def post_votes(request):
 
 @permission_required('shitbox.change_submission')
 def submissions_overview(request, page=1):
+    page = int(page)
     all_submissions = Submission.objects.all().order_by('-date').prefetch_related('author__profile')
-    paginator = Paginator(all_submissions, 20)
+    paginator = Paginator(all_submissions, 1)
 
     try:
         submissions = paginator.page(page)
@@ -39,8 +40,29 @@ def submissions_overview(request, page=1):
     except EmptyPage:
         submissions = paginator.page(paginator.num_pages)
 
+    useful_page_range = list(submissions.paginator.page_range)
+    limit_useful_page_range = []
+    
+    if page-2 in paginator.page_range:
+        limit_useful_page_range.append(page-2)
+    if page-1 in paginator.page_range:
+        limit_useful_page_range.append(page-1)
+    if page in paginator.page_range:
+        limit_useful_page_range.append(page)
+    if page+1 in paginator.page_range:
+        limit_useful_page_range.append(page+1)
+    if page+2 in paginator.page_range:
+        limit_useful_page_range.append(page+2)
+
+    len_range = len(list(submissions.paginator.page_range))
+    submissions.paginator.first_page = paginator.page(useful_page_range[0]).number
+    submissions.paginator.last_page = paginator.page(useful_page_range[-1]).number
+
     context = {
-        'submissions': submissions
+        'submissions': submissions,
+        'len_range': len_range,
+        'limit_useful_page_range': limit_useful_page_range,
+        'page': page,
     }
 
     return render(request, 'shitbox/list_submissions.html', context=context)
