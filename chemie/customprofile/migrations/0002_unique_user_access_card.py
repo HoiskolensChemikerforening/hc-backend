fef19d3c3f944ac15e18072bf2653e6a7ad58ba8
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-09-20 22:09:07 +0200

    Custom migration for making access cards unique Setting a blank access card value creates a random one

---
+++ chemie/customprofile/migrations/0002_unique_user_access_card.py
@@ -0,0 +1,42 @@
+from django.db import migrations, models
+import django.db.models.deletion
+from django.db.models import Count, Q
+
+
+def make_all_access_cards_unique(app, schema_editor):
+    try:
+        Profile = app.get_model('customprofile', 'Profile')
+    except LookupError:
+        print("Old model is no longer installed")
+        return
+
+    # Find all duplicate access card values
+    duplicate_values = Profile.objects\
+        .values('access_card')\
+        .annotate(Count('id'))\
+        .order_by()\
+        .filter(id__count__gt=1)
+
+    # Generate OR filter query based on these values
+    q_duplicate_or = Q(access_card='This string itself excludes all profiles. Will match every profile if left empty.')
+    for duplicate in duplicate_values:
+        duplicate = duplicate.get('access_card')
+        q_duplicate_or = q_duplicate_or | Q(access_card=duplicate)
+
+        # Finally: Set access_card to empty string and saving the object
+        # Model save function is called and will create a unique, and invalid (!), access card number
+    duplicates = Profile.objects.filter(q_duplicate_or)
+    print(duplicates.count())
+    for duplicate in duplicates:
+        duplicate.access_card = f'{duplicate.pk} - UGYLDIG KORTNUMMER'
+        duplicate.save()
+
+    print('NETTKOM')
+
+class Migration(migrations.Migration):
+    operations = [
+        migrations.RunPython(make_all_access_cards_unique, migrations.RunPython.noop),
+    ]
+    dependencies = [
+        ('customprofile', '0001_initial'),
+    ]
