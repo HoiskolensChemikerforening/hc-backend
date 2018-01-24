444e27391d364a4bff2ac5944821a4a3ac91e147
Eskild Ruud Mageli <eskild.emedd33@gmail.com>
2018-01-24 23:04:01 +0100

    Added Base64 converter for uploading of image to REST, with other work, it's posible to post image to website from mobile

--- chemie/shitbox/serializers.py
+++ chemie/shitbox/serializers.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
 from .models import Submission
+from django.core.files.base import ContentFile
+import base64
+import six
+import uuid
+import imghdr
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, six.string_types):
+            if 'data:' in data and ';base64,' in data:
+                header, data = data.split(';base64,')
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+            file_name = str(uuid.uuid4())[:12]  # 12 characters are more than enough.
+            file_extension = self.get_file_extension(file_name, decoded_file)
+            complete_file_name = "%s.%s" % (file_name, file_extension,)
+            data = ContentFile(decoded_file, name=complete_file_name)
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
 
 
 class SubmissionSerializer(serializers.ModelSerializer):
-
     author = serializers.ReadOnlyField(source='owner.username')
+    image = Base64ImageField(max_length=None, use_url=True)
 
     class Meta:
         model = Submission
