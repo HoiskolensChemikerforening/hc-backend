e4f4d71a42703a21cf32fe54b71c593543ef624f
Carl Johan Hambro <carljohan.hambro@gmail.com>
2018-02-09 21:13:47 +0100

    Refactored most style color variables

--- chemie/chemie/static/js/init.js
+++ chemie/chemie/static/js/init.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
 $('.modal').modal({
         dismissible: true, // Modal can be dismissed by clicking outside of the modal
         opacity: .5, // Opacity of modal background
-        inDuration: 300, // Transition in duration
-        outDuration: 200, // Transition out duration
-        startingTop: '4%', // Starting top style attribute
-        endingTop: '10%', // Ending top style attribute
+        inDuration: 200, // Transition in duration
+        outDuration: 100, // Transition out duration
+        startingTop: '2%', // Starting top style attribute
+        endingTop: '2%' // Ending top style attribute
     }
 );
