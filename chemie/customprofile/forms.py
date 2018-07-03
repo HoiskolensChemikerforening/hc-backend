c289e2ca18c838ddda6b22ccad3375823dfffcfe
Anja Rosvold From <anjarfrom@gmail.com>
2018-07-03 19:13:49 +0200

    Updated Login template to include modal with Terms n Cond when logging in

--- chemie/customprofile/forms.py
+++ chemie/customprofile/forms.py
@@ -182,5 +182,5 @@ class NameSearchForm(forms.Form):
 
 class ApprovedTermsForm(forms.Form):
     approval = forms.BooleanField(required=True,
-                                  label='Jeg godkjenner Vilkår og Betingelser', validators=
+                                  label='Jeg godkjenner ', validators=
                                   [lambda x: x == True])
