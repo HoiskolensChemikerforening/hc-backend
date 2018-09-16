bfced1cd3d5b34f1e59eec19b0feaac8793d2044
Carl Johan Hambro <carljohan.hambro@gmail.com>
2018-09-16 18:05:38 +0200

    Minor fixes

--- chemie/news/views.py
+++ chemie/news/views.py
@@ -5,18 +5,18 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
 
 from .models import Article
-from .forms import NewsPost
+from .forms import ArticleForm
 
 
 @permission_required('news.add_article')
 def create_post(request):
-    post = NewsPost(request.POST or None, request.FILES or None)
+    post = ArticleForm(request.POST or None, request.FILES or None)
     if request.POST:
         if post.is_valid():
             instance = post.save(commit=False)
             instance.author = request.user
             instance.save()
-            messages.add_message(request, messages.WARNING, 'Nyheten ble opprettet', extra_tags='Opprettet')
+            messages.add_message(request, messages.SUCCESS, 'Nyheten ble opprettet', extra_tags='Opprettet')
             return HttpResponseRedirect(reverse('news:index'))
     context = {
         'post': post
@@ -45,24 +45,23 @@ def delete_article(request, article_id, slug):
     article = get_object_or_404(Article, id=article_id)
     article.published = False
     article.save()
-    messages.add_message(request, messages.WARNING, 'Nyheten ble slettet', extra_tags='Slettet')
+    messages.add_message(request, messages.SUCCESS, 'Nyheten ble slettet', extra_tags='Slettet')
     return HttpResponseRedirect(reverse("news:index"))
 
 @permission_required("news.change_article")
 def edit_article(request, article_id, slug):
     article = get_object_or_404(Article, id=article_id)
+    post = ArticleForm(request.POST or None, instance=article)
+
     if request.method == 'POST':
-        post = NewsPost(request.POST, instance=article)
-        try:
-            if post.is_valid():
-                post.save()
-                messages.add_message(request, messages.WARNING, 'Nyheten ble endret', extra_tags='Endret')
-                return HttpResponseRedirect(reverse('news:index'))
-        except Exception as e:
-            messages.add_message(request, messages.WARNING, 'Nyheten ble ikke endret på grunn av et problem: {}'.format(e), extra_tags='Feil')
-    else:
-        post = NewsPost(instance=article)
+
+        if post.is_valid():
+            post.save()
+            messages.add_message(request, messages.SUCCESS, 'Nyheten ble endret', extra_tags='Endret')
+            return HttpResponseRedirect(reverse('news:index'))
+
     context = {
         'post': post,
     }
+
     return render(request, 'news/create_post.html', context)
