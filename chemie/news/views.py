754b369adf15e540c6ffacdfd2e24b058a53a0f1
Carl Johan Hambro <carljohan.hambro@gmail.com>
2018-09-16 18:16:35 +0200

    Minor fixes

--- chemie/news/views.py
+++ chemie/news/views.py
@@ -1,52 +1,65 @@
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse, reverse_lazy
-from django.shortcuts import get_object_or_404, render
-from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
+from django.contrib.auth.decorators import permission_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
-from .models import Article
 from .forms import ArticleForm
+from .models import Article
 
 
 @permission_required('news.add_article')
 def create_post(request):
+
     post = ArticleForm(request.POST or None, request.FILES or None)
+
     if request.POST:
         if post.is_valid():
+
             instance = post.save(commit=False)
             instance.author = request.user
             instance.save()
             messages.add_message(request, messages.SUCCESS, 'Nyheten ble opprettet', extra_tags='Opprettet')
+
             return HttpResponseRedirect(reverse('news:index'))
+
     context = {
         'post': post
     }
+
     return render(request, 'news/create_post.html', context)
 
 
 def news_details(request, article_id, slug):
     article = get_object_or_404(Article, id=article_id)
+
     context = {
         'article': article
     }
+
     return render(request, 'news/detail.html', context)
 
 
 def list_all(request):
     all_posts = Article.objects.filter(published=True).order_by('-published_date')
+
     context = {
         'posts': all_posts,
     }
+
     return render(request, 'news/list.html', context)
 
 
 @permission_required("news.delete_article")
 def delete_article(request, article_id, slug):
+
     article = get_object_or_404(Article, id=article_id)
     article.published = False
     article.save()
     messages.add_message(request, messages.SUCCESS, 'Nyheten ble slettet', extra_tags='Slettet')
+
     return HttpResponseRedirect(reverse("news:index"))
+
 
 @permission_required("news.change_article")
 def edit_article(request, article_id, slug):
