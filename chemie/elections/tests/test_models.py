4fd0f706254e71299269ee68b853eb884c0bab8b
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2018-09-18 20:54:25 +0200

    Cleaned up get_current_position_winners and tested it to give the correct winners

--- chemie/elections/tests/test_models.py
+++ chemie/elections/tests/test_models.py
@@ -1,7 +1,5 @@
 import pytest
-from ..models import Candidates, Position
-from .factories import CandidateFactory
-
+from django.core.exceptions import ObjectDoesNotExist
 
 @pytest.mark.django_db
 def test_inital_election(create_election_with_positions):
@@ -49,17 +47,17 @@ def test_delete_single_candidate(create_election_with_positions, create_candidat
     election, positions = create_election_with_positions
     election.add_position(positions)
 
-    new_candidates = create_candidates
     candidates = create_candidates
     position = positions[0]
     position.candidates.add(*candidates)
     candidate = candidates[0]
     position.candidates.remove(candidate)
     assert candidate not in list(position.candidates.all())
-    position.candidates.add(new_candidates[0])
+    position.candidates.add(candidate)
     assert candidate in list(position.candidates.all())
     position.delete_candidates(candidate)
-    assert not candidate
+    with pytest.raises(ObjectDoesNotExist):
+        candidate.refresh_from_db()
 
 
 @pytest.mark.django_db
@@ -73,7 +71,32 @@ def test_delete_position(create_election_with_positions, create_candidates):
     election.delete_position(positions)
 
     # Refetch all objects, since they have been deleted, and references are stale
-    candidates = Candidates.objects.all()
-    positions = election.positions.all()
-    assert positions.count() is 0
-    assert candidates.count() is 0
+    with pytest.raises(ObjectDoesNotExist):
+        for position in positions:
+            position.refresh_from_db()
+        for candidate in candidates:
+            candidate.refresh_from_db()
+
+@pytest.mark.django_db
+def test_get_current_position_winners(create_election_with_positions, create_candidates):
+    election, positions = create_election_with_positions
+    candidates = create_candidates
+
+    position = positions[0]
+    number_of_winners = position.spots
+    winner_candidates = []
+    for i in range(number_of_winners):
+        candidates[i].votes = i + 1
+        candidates[i].save()
+        winner_candidates.append(candidates[i])
+
+    position.candidates.add(*candidates)
+    position.get_current_position_winners()
+    assert list(position.winners.all()) == list(winner_candidates)
+
+
+
+
+
+
+
