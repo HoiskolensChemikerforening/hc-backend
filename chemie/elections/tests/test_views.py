cc0a2d7bfd93be49d1e56e153fd4f68c2227588d
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2018-09-22 15:15:08 +0200

    Split admin views and regular views in two files. Checked that users are getting correct statuses and redirects when election is open and not open

--- chemie/elections/tests/test_views.py
+++ chemie/elections/tests/test_views.py
@@ -1,22 +1,21 @@
 import pytest
 from django.shortcuts import reverse
-from
-
 
 
 @pytest.mark.django_db
-def test_user_not_logged_in(client,create_user):
+def test_user_not_logged_in(client):
     request = client.get(reverse('elections:vote'))
     assert request.status_code == 302
     assert reverse('login') in request.url
 
+
 @pytest.mark.django_db
-def test_election_not_open(client,create_user):
+def test_election_not_open(client, create_user):
     user = create_user
     client.login(username=user.username, password='defaultpassword')
 
     request = client.get(reverse('elections:vote'))
-    assert "Valget har ikke åpnet enda" in request.content.decode('utf-8')
+    assert 'Valget har ikke åpnet enda' in request.content.decode('utf-8')
     assert '<a class="button" href="/elections/results">' in request.content.decode('utf-8')
 
     request = client.get(reverse('elections:has_voted'))
@@ -28,12 +27,15 @@ def test_election_not_open(client,create_user):
 
 
 @pytest.mark.django_db
-def test_election_is_open(client,create_user, create_election_with_positions):
+def test_election_is_open(client, create_user, create_election_with_positions):
     election, positions = create_election_with_positions
-    election = Election.objects.create()
+    election.is_open = True
+    election.save()
+    user = create_user
+
     client.login(username=user.username, password='defaultpassword')
     request = client.get(reverse('elections:vote'))
-    assert reverse('elections:vote') == request.url
+    assert request.status_code is 200
     assert "Klargjøres til valg" in request.content.decode('utf-8')
 
     request = client.get(reverse('elections:has_voted'))
@@ -42,7 +44,3 @@ def test_election_is_open(client,create_user, create_election_with_positions):
     request = client.get(reverse('elections:voting'))
     assert request.status_code == 302
     assert reverse('elections:vote') == request.url
-
-
-
-
