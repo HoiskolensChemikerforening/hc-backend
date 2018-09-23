3f62a7f0588ad362574e0aaea468481e30dfe14d
Eskild Ruud Mageli <eskild.emedd33@gmail.com>
2018-09-23 16:20:16 +0200

    [WIP] Added test_function and refactored model functions

--- chemie/elections/urls.py
+++ chemie/elections/urls.py
@@ -7,8 +7,8 @@ admin_urlpatterns = [
     url(r'^admin/end/$', views.admin_end_election, name='admin_end_election'),
     url(r'^admin/register/$', views.admin_register_positions, name='admin_register_positions'),
     url(r'^admin/register/(?P<pk>\d+)/$', views.admin_register_candidates, name='admin_register_candidates'),
-    url(r'^admin/register/(?P<pk>\d+)/start', views.admin_start_voting, name='admin_start_voting'),
-    url(r'^admin/register/results', views.admin_results, name='admin_results'),
+    url(r'^admin/register/(?P<pk>\d+)/start', views.admin_voting_is_active, name='admin_start_voting'),
+    url(r'^admin/register/(?P<pk>\d+)/results', views.admin_results, name='admin_results'),
     url(r'^user-autocomplete/', UserAutocomplete.as_view(), name='user-autocomplete',),
 ]
 user_urlpatterns = [
