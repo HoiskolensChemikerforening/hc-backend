73b7134d4a0e58fd73110e98d6379abd5e6e5058
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2018-09-24 17:34:13 +0200

    Added some new tests to check if user can vote for multiple users and vote blank.

--- chemie/elections/tests/test_views.py
+++ chemie/elections/tests/test_views.py
@@ -68,7 +68,7 @@ def test_vote_for_one_user(client, create_user, create_election_with_positions, 
     client.login(username=user.username, password='defaultpassword')
     request = client.post(
         reverse('elections:voting'),
-        {'candidates': [str(candidate.id)]}
+        {'candidates': candidate.id}
     )
     assert request.url == reverse('elections:has_voted')
     candidate.refresh_from_db()
@@ -79,3 +79,92 @@ def test_vote_for_one_user(client, create_user, create_election_with_positions, 
     user.profile.refresh_from_db()
     assert user.profile.voted is True
 
+
+@pytest.mark.django_db
+def test_vote_for_multiple_users(client, create_user, create_election_with_positions, create_candidates):
+    election, positions = create_election_with_positions
+    candidates = create_candidates
+    election.is_open = True
+    candidate, position = candidates[0], positions[0]
+    position.candidates.add(*candidates)
+    election.add_position(position)
+    election.start_current_election(position)
+    election.save()
+    election.refresh_from_db()
+
+    number_of_winners = position.spots
+    winner_candidates = []
+    for i in range(number_of_winners):
+        winner_candidates.append(candidates[i])
+
+    user = create_user
+    client.login(username=user.username, password='defaultpassword')
+    client.post(
+        reverse('elections:voting'),
+        {'candidates': [cand.id for cand in winner_candidates]}
+    )
+    for cand in winner_candidates:
+        cand.refresh_from_db()
+        assert cand.votes is 1
+    for cand in candidates[number_of_winners+1:]:
+        cand.refresh_from_db()
+        assert cand.votes is 0
+    position.refresh_from_db()
+    assert position.total_votes is number_of_winners
+    user.profile.refresh_from_db()
+    assert user.profile.voted is True
+
+
+@pytest.mark.django_db
+def test_vote_blank(client, create_user, create_election_with_positions, create_candidates):
+    election, positions = create_election_with_positions
+    candidates = create_candidates
+    election.is_open = True
+    position = positions[0]
+    position.candidates.add(*candidates)
+    election.add_position(position)
+    election.start_current_election(position)
+    election.save()
+    election.refresh_from_db()
+
+    user = create_user
+    client.login(username=user.username, password='defaultpassword')
+    request = client.post(
+        reverse('elections:voting'),
+        {'Blank': 'Stem blankt'}
+    )
+    for cand in candidates:
+        cand.refresh_from_db()
+        assert cand.votes is 0
+    position.refresh_from_db()
+    assert position.total_votes is 1
+    user.profile.refresh_from_db()
+    assert user.profile.voted is True
+
+
+# @pytest.mark.django_db
+# def test_invalid_vote(client, create_user, create_election_with_positions, create_candidates):
+#     election, positions = create_election_with_positions
+#     candidates = create_candidates
+#     election.is_open = True
+#     position = positions[0]
+#     position.candidates.add(*candidates)
+#     election.add_position(position)
+#     election.start_current_election(position)
+#     election.save()
+#     election.refresh_from_db()
+#
+#     user = create_user
+#     client.login(username=user.username, password='defaultpassword')
+#     # Trying to vote for all candidates, which are more candidates than position spots
+#     request = client.post(
+#         reverse('elections:voting'),
+#         {'candidates': [cand.id for cand in candidates]}
+#     )
+#     """
+#     client.post(
+#         reverse('elections:voting'),
+#         {'candidates': [cand.id for cand in winner_candidates]}
+#     )
+#     """
+#     assert 'Gjør det rett a kis' in request.content.decode('utf-8')
