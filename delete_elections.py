2e0b4624155a5170249ff51c50fdb3ca84f2cbc3
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2018-10-03 21:30:59 +0200

    Edited gitignore and removed cluttering python file

--- delete_elections.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'chemie.chemie.settings'
-from django import setup
-setup()
-from chemie.elections.models import Election, Position, Candidate
-
-Election.objects.all().delete()
-Position.objects.all().delete()
-Candidate.objects.all().delete()
