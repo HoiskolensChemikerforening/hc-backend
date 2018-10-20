47e379ce6026d5be6a63a81456cf4b2d7928ea14
Eskild Ruud Mageli <eskild.emedd33@gmail.com>
2018-10-20 16:30:37 +0200

    Send push notification to user when new article is created

--- chemie/news/views.py
+++ chemie/news/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import permission_required
-
+from chemie.customprofile.push_notification import send
 from .models import Article
 from .forms import ArticleForm
 
@@ -17,6 +17,7 @@ def create_post(request):
             instance = post.save(commit=False)
             instance.author = request.user
             instance.save()
+            send(post.cleaned_data['title'],"NYHET!",None) #Send push notification to user
             return HttpResponseRedirect(reverse('news:index'))
     context = {
         'post': post
