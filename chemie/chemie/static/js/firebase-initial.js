c5ecb23c7731cdc13ca5764c75693dedda4953a7
Eskild Ruud Mageli <eskild.emedd33@gmail.com>
2018-10-23 00:00:24 +0200

    Write documentation. Reorganize structure.

--- chemie/chemie/static/js/firebase-initial.js
+++ chemie/chemie/static/js/firebase-initial.js
@@ -1,4 +1,45 @@
-//TODO populate this script with the one in base.html
+// Main script
+//----------------------------------------
+var config = { // Firebase front-end configurations
+    apiKey: "AIzaSyBiK7kDOZlPR_6QQyX8pxsrY3RHtlSKewQ",
+    authDomain: "chemie-4a9d7.firebaseapp.com",
+    databaseURL: "https://chemie-4a9d7.firebaseio.com",
+    projectId: "chemie-4a9d7",
+    storageBucket: "chemie-4a9d7.appspot.com",
+    messagingSenderId: "949677257241"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+navigator.serviceWorker.register('/static/js/firebase-messaging-sw.js')
+    .then(function (registration) {
+        messaging.useServiceWorker(registration);
+        if (window.location.href == "https://chemie.no") { //Only prompts permission on home page
+            messaging.requestPermission()
+                .then(function () {
+                    return messaging.getToken();
+                }).then(function (token) {
+                // Saving device token backend to be used when notification is to be sent
+                var browser = getBrowser();
+                postAjax("web_notifications/", {'token': token, 'browser': browser});
+            });
+        }
+    })
+    .catch(function (err) {
+        console.log(("Error Occured:"));
+    });
+
+messaging.onMessage(function (payload) {
+    // When user is present on the site, they are presented with a toast message instead of the notification.
+    var snackbar = document.getElementById("snackbar");
+    snackbar.innerHTML = payload.notification.body;
+    snackbar.className = "show";
+    setTimeout(function () {
+        snackbar.className = snackbar.className.replace("show", "");
+    }, 3000);
+});
+
+// Functions
+//-----------------------------------------------------------
 
 //Gives the CSRF token to be used in a AJAX POST request
 function getCookie(name) {
@@ -16,76 +57,47 @@ function getCookie(name) {
     }
     return cookieValue
 }
+
 function postAjax(url, data) {
     var params = typeof data == 'string' ? data : Object.keys(data).map(
-            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-        ).join('&');
+        function (k) {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        }
+    ).join('&');
 
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
     xhr.open('POST', url);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) { console.log(xhr.status); }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState > 3 && xhr.status == 200) {
+            console.log(xhr.status);
+        }
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     var csrftoken = getCookie('csrftoken');
-    xhr.setRequestHeader("X-CSRFToken",csrftoken);
+    xhr.setRequestHeader("X-CSRFToken", csrftoken);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.send(params);
     return xhr
 }
 
-var config = {
-    apiKey: "AIzaSyBiK7kDOZlPR_6QQyX8pxsrY3RHtlSKewQ",
-    authDomain: "chemie-4a9d7.firebaseapp.com",
-    databaseURL: "https://chemie-4a9d7.firebaseio.com",
-    projectId: "chemie-4a9d7",
-    storageBucket: "chemie-4a9d7.appspot.com",
-    messagingSenderId: "949677257241"
-};
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
-navigator.serviceWorker.register('/static/js/firebase-messaging-sw.js')
-.then(function(registration) {
-  messaging.useServiceWorker(registration);
-  //TODO change the adress to chemie.no in production
-  if (window.location.href == "http://127.0.0.1:8000/"){ //only prompts permission on home page
-    messaging.requestPermission()
-    .then( function(){
-    return messaging.getToken();
-  }).then(function (token) {
-    // Saving device token backend to be used when notification is to be sent
-    var browser = getBrowser()
-    postAjax("web_notifications/",{'token':token,'browser':browser});
-});
-    }
-  })
-  .catch( function(err){
-      console.log(("Error Occured"));
-  });
 
-
-
-function getBrowser(){
+function getBrowser() {
     var browser = null
-     if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 )
-    {
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
         browser = 'Opera'
     }
-    else if(navigator.userAgent.indexOf("Chrome") != -1 )
-    {
+    else if (navigator.userAgent.indexOf("Chrome") != -1) {
         browser = 'Chrome'
     }
-    else if(navigator.userAgent.indexOf("Safari") != -1)
-    {
+    else if (navigator.userAgent.indexOf("Safari") != -1) {
         browser = 'Safari'
     }
-    else if(navigator.userAgent.indexOf("Firefox") != -1 )
-    {
-         browser = 'Firefox'
+    else if (navigator.userAgent.indexOf("Firefox") != -1) {
+        browser = 'Firefox'
     }
-    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
     {
-      browser = 'IE'
+        browser = 'IE'
     }
     return browser
 }
