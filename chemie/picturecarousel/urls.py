e554bce0f74e50a885df78b8aa958c4f5a0c025b
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-01-06 19:39:22 +0100

    Black reformatting picturecarousel app (auto reformatted, no coding myself)

--- chemie/picturecarousel/urls.py
+++ chemie/picturecarousel/urls.py
@@ -2,12 +2,21 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'picturecarousel'
+app_name = "picturecarousel"
 
 urlpatterns = [
-    url(r'^send/', views.submit_picture, name="submit"),
-    url(r'^$', views.view_carousel, name='display'),
-    url(r'^overview/$', views.approve_pictures, name="overview"),
-    url(r'^overview/approve/(?P<picture_id>[0-9]+)', views.approve_deny, name="approve"),
-    url(r'^overview/deny/(?P<picture_id>[0-9]+)', views.approve_deny, {'deny': True}, name="deny"),
+    url(r"^send/", views.submit_picture, name="submit"),
+    url(r"^$", views.view_carousel, name="display"),
+    url(r"^overview/$", views.approve_pictures, name="overview"),
+    url(
+        r"^overview/approve/(?P<picture_id>[0-9]+)",
+        views.approve_deny,
+        name="approve",
+    ),
+    url(
+        r"^overview/deny/(?P<picture_id>[0-9]+)",
+        views.approve_deny,
+        {"deny": True},
+        name="deny",
+    ),
 ]
