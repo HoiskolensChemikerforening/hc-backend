dfa8a44714ce1b30e584c65a1246312f4a55f20e
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-01-06 19:42:15 +0100

    Black reformatting yearbook app (auto reformatted, no coding myself)

--- chemie/yearbook/urls.py
+++ chemie/yearbook/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'yearbook'
+app_name = "yearbook"
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<year>[0-9]+)/$', views.index),
+    url(r"^$", views.index, name="index"),
+    url(r"^(?P<year>[0-9]+)/$", views.index),
 ]
