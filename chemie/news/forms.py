415d46512f3cd01d787a98315e80a69eaa8fe510
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-01-06 19:38:34 +0100

    Black reformatting news app (auto reformatted, no coding myself)

--- chemie/news/forms.py
+++ chemie/news/forms.py
@@ -4,14 +4,8 @@ from .models import Article
 
 
 class ArticleForm(forms.ModelForm):
-    layout = M.Layout(M.Row('title'),
-                      M.Row('content'),
-                      M.Row('image'),
-                      )
+    layout = M.Layout(M.Row("title"), M.Row("content"), M.Row("image"))
+
     class Meta:
         model = Article
-        fields = [
-            'title',
-            'content',
-            'image',
-        ]
+        fields = ["title", "content", "image"]
