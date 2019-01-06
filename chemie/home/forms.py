327de86d7cdfb6ef3d49ebf875dd5e2176450c9d
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-01-06 19:35:54 +0100

    Black reformatting home app (auto reformatted, no coding myself)

--- chemie/home/forms.py
+++ chemie/home/forms.py
@@ -8,30 +8,43 @@ from .models import OfficeApplication
 
 
 class ContactForm(forms.Form):
-    layout = M.Layout(M.Row('contact_name', 'contact_email'),
-                      M.Row('content'),
-                      M.Row('captcha')
-                      )
+    layout = M.Layout(
+        M.Row("contact_name", "contact_email"),
+        M.Row("content"),
+        M.Row("captcha"),
+    )
     captcha = ReCaptchaField()
     contact_name = forms.CharField(required=False, label="Navn")
     contact_email = forms.EmailField(required=False, label="E-post")
-    content = forms.CharField(required=True, widget=forms.Textarea, label="Melding")
+    content = forms.CharField(
+        required=True, widget=forms.Textarea, label="Melding"
+    )
 
 
 class PostFundsForm(forms.ModelForm):
-    layout = M.Layout(M.Row('applier', 'bank_account_holder'),
-                      M.Row('price_range', 'bank_account_id'),
-                      M.Row('purpose'),
-                      M.Row('description'),
-                      M.Row('receipt'),
-                      )
+    layout = M.Layout(
+        M.Row("applier", "bank_account_holder"),
+        M.Row("price_range", "bank_account_id"),
+        M.Row("purpose"),
+        M.Row("description"),
+        M.Row("receipt"),
+    )
 
     class Meta:
         model = FundsApplication
-        widgets = {'price_range': forms.RadioSelect,
-                   'description': forms.Textarea(attrs={
-                       'placeholder': 'Beskrivelse av formålet, dato, hvor, hvor mye midler det søkes på, hva pengene skal brukes på, osv.'}),
-                   'purpose': forms.TextInput(attrs={'placeholder': 'Kort beskrivelse av det det søkes midler til'})}
+        widgets = {
+            "price_range": forms.RadioSelect,
+            "description": forms.Textarea(
+                attrs={
+                    "placeholder": "Beskrivelse av formålet, dato, hvor, hvor mye midler det søkes på, hva pengene skal brukes på, osv."
+                }
+            ),
+            "purpose": forms.TextInput(
+                attrs={
+                    "placeholder": "Kort beskrivelse av det det søkes midler til"
+                }
+            ),
+        }
 
         fields = [
             "applier",
@@ -51,7 +64,8 @@ class PostOfficeForms(forms.ModelForm):
 
 
 class FlatpageEditForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget(config_name='flatpages'))
+    content = forms.CharField(widget=CKEditorWidget(config_name="flatpages"))
+
     class Meta:
         model = FlatPage
-        fields = ('content',)
+        fields = ("content",)
