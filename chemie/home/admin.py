098632c317610e721e9cc091ed90390be48b5a94
Anja Rosvold From <anjarfrom@gmail.com>
2019-01-29 15:16:18 +0100

    Removed access card from OfficeApplication. All users can apply once a month.

--- chemie/home/admin.py
+++ chemie/home/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import FundsApplication
+from .models import FundsApplication, OfficeApplication
 
 admin.site.register(FundsApplication)
+
+@admin.register(OfficeApplication)
+class OfficeApplication(admin.ModelAdmin):
+    ordering = ('-created',)
+    list_display = ('author', 'student_username', 'created')
