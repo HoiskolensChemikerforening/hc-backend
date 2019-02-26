f7a31fc69f1af3296a56b4f2f26b552088152c62
Geir Hovde <geirhov97@gmail.com>
2019-02-26 19:17:15 +0100

    Add urls to quiz

--- chemie/quiz/views.py
+++ chemie/quiz/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hei <3")
+
+
+def quiz_term(request, pk):
+    return HttpResponse("Verdens beste musikknettside" + str(pk))
