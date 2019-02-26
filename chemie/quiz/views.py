a8d033dbb05d67e9e1bccc0ac75f005628ddb9f2
bendissa <skolebolla3@gmail.com>
2019-02-26 22:04:22 +0100

    Add Quiz term detail view and template

--- chemie/quiz/views.py
+++ chemie/quiz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import QuizTerm, QuizScore
 # Create your views here.
 
 
@@ -8,4 +10,10 @@ def index(request):
 
 
 def quiz_term(request, pk):
-    return HttpResponse("Verdens beste musikknettside" + str(pk))
+    term = get_object_or_404(QuizTerm, pk=pk)
+    scores = QuizScore.objects.filter(term=term).order_by('-score')
+    context = {
+        'term': term,
+        'scores': scores
+    }
+    return render(request, 'quiz/term_detail.html', context)
