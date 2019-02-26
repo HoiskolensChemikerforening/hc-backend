8b360543af74b43e1f5c1ac3f9c9b6efd9905766
bendissa <skolebolla3@gmail.com>
2019-02-26 18:18:21 +0100

    Add models QuizTerm and QuizScore

--- chemie/quiz/models.py
+++ chemie/quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class QuizTerm(models.Model):
+    is_active = models.BooleanField()
+    term = models.CharField(max_length=100)
+
+
+class QuizScore(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    term = models.ForeignKey(QuizTerm, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
