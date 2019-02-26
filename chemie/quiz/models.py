ef810ae4a134989dd81a16041963ee0989f35b9b
bendissa <skolebolla3@gmail.com>
2019-02-26 22:04:22 +0100

    Add Quiz term detail view and template

--- chemie/quiz/models.py
+++ chemie/quiz/models.py
@@ -9,8 +9,14 @@ class QuizTerm(models.Model):
     is_active = models.BooleanField()
     term = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.term
+
 
 class QuizScore(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     term = models.ForeignKey(QuizTerm, on_delete=models.CASCADE)
     score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.score)
