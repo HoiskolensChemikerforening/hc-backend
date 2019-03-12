fe5c9ffd3abb46c1102fb13a0e57323813baaa94
bendissa <skolebolla3@gmail.com>
2019-03-12 19:30:50 +0100

    Model stuff

--- chemie/corporate/models.py
+++ chemie/corporate/models.py
@@ -3,11 +3,11 @@ from sorl.thumbnail import ImageField
 
 
 SPECIALIZATIONS = ((1, 'Bioteknologi'),
-                (2, 'Organisk kjemi'),
-                (3, 'Anvendt teoretisk kjemi'),
-                (4, 'Analytisk kjemi'),
-                (5, 'Kjemisk prosessteknologi'),
-                (6, 'Materialkjemi og energiteknologi'))
+                   (2, 'Organisk kjemi'),
+                   (3, 'Anvendt teoretisk kjemi'),
+                   (4, 'Analytisk kjemi'),
+                   (5, 'Kjemisk prosessteknologi'),
+                   (6, 'Materialkjemi og energiteknologi'))
 
 
 class Interview(models.Model):
@@ -19,9 +19,11 @@ class Interview(models.Model):
     picture = models.ImageField()
 
 
-
 class Company(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     logo = models.ImageField(upload_to="corporate", verbose_name="Logo")
     #specializations = models.(choices=SPECIALIZATIONS)
+
+    def __str__(self):
+        return self.name
