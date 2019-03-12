4872b272f3d2b63b6b10388343805255a0eea8d3
bendissa <skolebolla3@gmail.com>
2019-03-12 18:27:22 +0100

    Basic index page

--- chemie/corporate/urls.py
+++ chemie/corporate/urls.py
@@ -1,3 +1,10 @@
 from django.conf.urls import url
+from . import views
 
 app_name = "corporate"
+
+urlpatterns = [
+    url(r"^$",
+        views.index,
+        name="index"),
+]
--- chemie/corporate/views.py
+++ chemie/corporate/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Du er sykt heft10")
