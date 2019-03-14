46488790b7cee56b9bd1a8025108e6dcb385f0a0
bendissa <skolebolla3@gmail.com>
2019-03-14 11:35:30 +0100

    Add PictureTagForm

--- chemie/picturecarousel/forms.py
+++ chemie/picturecarousel/forms.py
@@ -1,11 +1,42 @@
 from django import forms
-from .models import Contribution
+from dal import autocomplete
+from .models import Contribution, PictureTag
 import material as M
 
 
 class Pictureform(forms.ModelForm):
-    layout = M.Layout(M.Row("image"))
+    layout = M.Layout(M.Row('image'),
+                      M.Row('tags'))
 
     class Meta:
         model = Contribution
-        fields = ["image"]
+        fields = ["image",
+                  "tags"]
+
+        widgets = {
+            'tags': autocomplete.ModelSelect2Multiple(
+                url='picturecarousel:picturetag-autocomplete',
+                attrs={
+                    'style': 'width: 10px;'
+                }
+            )
+        }
+
+
+class PictureTagForm(forms.ModelForm):
+    class Meta:
+        model = Contribution
+        fields = ['tags']
+
+        widgets = {
+            'tags': autocomplete.ModelSelect2Multiple(
+                url='picturecarousel:picturetag-autocomplete',
+                attrs={
+                    'style': 'width: 10px;',
+                }
+            )
+        }
+
+        labels = {
+            'tags': ''
+        }
