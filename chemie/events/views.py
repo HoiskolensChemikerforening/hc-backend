adb90a35dd18b289a7490a02526bd153859dece4
Carl Johan Hambro <carljohan.hambro@gmail.com>
2019-03-19 19:31:37 +0100

    Add waiting list ordering by date for Bedpres and Social-events

--- chemie/events/views.py
+++ chemie/events/views.py
@@ -216,9 +216,11 @@ class ViewSocialDetailsView(DetailView):
         confirmed = attendees.filter(
             event=self.object, status=REGISTRATION_STATUS.CONFIRMED
         )
-        waiting = attendees.filter(
-            event=self.object, status=REGISTRATION_STATUS.WAITING
-        )
+
+        waiting = attendees \
+            .filter(event=self.object, status=REGISTRATION_STATUS.WAITING) \
+            .order_by('created')
+
         context.update({"attendees": confirmed, "waiting_list": waiting})
         return context
 
