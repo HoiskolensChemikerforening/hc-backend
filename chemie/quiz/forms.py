5b5e4691510cf90d2cae27261b7cc148c6884ac8
bendissa <bendik.sannes@gmail.com>
2019-03-31 17:36:55 +0200

    Form for creating new quiz

--- chemie/quiz/forms.py
+++ chemie/quiz/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import QuizScore
+from .models import QuizScore, QuizTerm
 from dal import autocomplete
 
 
@@ -16,3 +16,25 @@ class QuizScoreForm(forms.ModelForm):
                     'style': 'width:10ch; margin-left: 10px;'
                 })
         }
+
+
+class CreateQuizTermForm(forms.ModelForm):
+    class Meta:
+        model = QuizTerm
+        fields = ['term', 'is_active']
+        widgets = {
+            'is_active': forms.CheckboxInput()
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        term = cleaned_data.get('term')
+        is_active = cleaned_data.get('is_active')
+
+        if term and is_active:
+            active_quiz = QuizTerm.objects.get(is_active=True)
+            if active_quiz:
+                raise forms.ValidationError(
+                    "Det finnes allerede en aktiv Quiz: %s. "
+                    "Deaktiver den før du oppretter en ny" % active_quiz.term
+                )
