e11ad950867210de10450f44f5b0e919029f91f7
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-04-01 22:09:47 +0200

    Fix edit url to point to correct view. Previously, another url overshadowed the correct one

--- chemie/shop/urls.py
+++ chemie/shop/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r"^$", views.index, name="index"),
     url(r"^admin/$", views.admin, name="admin"),
     url(r"^admin/refill-balance", views.refill, name="refill"),
-    url(r"^admin/add-item/", views.add_item, name="add-item"),
+    url(r"^admin/add-item/$", views.add_item, name="add-item"),
     url(
         r"^admin/add-item/edit/(?P<pk>[0-9]+)",
         views.edit_item,
