d75b9791b44daff2025d97e7036f65a428b75ff3
bendissa <bendik.sannes@gmail.com>
2019-04-03 00:15:46 +0200

    Prevent more than one active Quiz by overriding the model's save method rather than in the

--- chemie/quiz/forms.py
+++ chemie/quiz/forms.py
@@ -22,19 +22,3 @@ class CreateQuizTermForm(forms.ModelForm):
     class Meta:
         model = QuizTerm
         fields = ['term', 'is_active']
-        widgets = {
-            'is_active': forms.CheckboxInput()
-        }
-
-    def clean(self):
-        cleaned_data = super().clean()
-        term = cleaned_data.get('term')
-        is_active = cleaned_data.get('is_active')
-
-        if term and is_active:
-            active_quiz = QuizTerm.objects.get(is_active=True)
-            if active_quiz:
-                raise forms.ValidationError(
-                    "Det finnes allerede en aktiv Quiz: %s. "
-                    "Deaktiver den før du oppretter en ny" % active_quiz.term
-                )
