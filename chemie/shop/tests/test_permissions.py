3feafecf0d054703ebb6fb550bb854fe3ab39abf
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-04-06 16:59:23 +0200

    Factor out a lot of code that has nothing to do with permissions, but rather creating of objects.

--- chemie/shop/tests/test_permissions.py
+++ chemie/shop/tests/test_permissions.py
@@ -1,34 +1,26 @@
-from decimal import Decimal
-
 import pytest
 from django.shortcuts import reverse
-from chemie.customprofile.models import Profile
+from chemie.shop.urls import urlpatterns
 
-MAX_AMOUNT = 10**4
+MAX_AMOUNT = 10 ** 4
 
 
-#################################
-############# VIEWS #############
-#################################
+# --------------------------------- #
+# ------------- VIEWS ------------- #
+# --------------------------------- #
 
-########### Category ############
+
+# ----------- Category ----------- #
 @pytest.mark.django_db
-def test_create_category_all_perms(client, create_user_all_perms):
+def test_view_create_page_category_all_perms(client, create_user_all_perms):
     user = create_user_all_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-category"))
     assert request.status_code == 200
-    request = client.post(
-        reverse("shop:add-category"),
-        data={
-            "name": "Kategori",
-        }
-    )
-    assert request.status_code == 302
 
 
 @pytest.mark.django_db
-def test_create_category_item_perms(client, create_user_item_perms):
+def test_view_create_page_category_item_perms(client, create_user_item_perms):
     user = create_user_item_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-category"))
@@ -37,22 +29,19 @@ def test_create_category_item_perms(client, create_user_item_perms):
 
 
 @pytest.mark.django_db
-def test_create_category_category_perms(client, create_user_category_perms):
+def test_view_create_page_category_category_perms(
+    client, create_user_category_perms
+):
     user = create_user_category_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-category"))
     assert request.status_code == 200
-    request = client.post(
-        reverse("shop:add-category"),
-        data={
-            "name": "Kategori",
-        }
-    )
-    assert request.status_code == 302
 
 
 @pytest.mark.django_db
-def test_create_category_refill_perms(client, create_user_refill_perms):
+def test_view_create_page_category_refill_perms(
+    client, create_user_refill_perms
+):
     user = create_user_refill_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-category"))
@@ -60,64 +49,29 @@ def test_create_category_refill_perms(client, create_user_refill_perms):
     assert "/login" in request.url
 
 
+# ----------- Items ----------- #
 @pytest.mark.django_db
-def test_create_category_no_perms(client, create_user_no_perms):
-    user = create_user_no_perms
-    client.login(username=user.username, password="defaultpassword")
-    request = client.get(reverse("shop:add-category"))
-    assert request.status_code == 302
-    assert "/login" in request.url
-
-
-############# Items #############
-@pytest.mark.django_db
-def test_create_item_with_item_perms(client, create_user_all_perms, create_category, create_image):
+def test_view_create_page_item_with_all_perms(client, create_user_all_perms):
     user = create_user_all_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-item"))
     assert request.status_code == 200
-    image = create_image
-    cat = create_category
-    request = client.post(
-        reverse("shop:add-item"),
-        follow=True,
-        data={
-            "name": "Vare",
-            "price": Decimal(10),
-            "description": "Beskrivelse",
-            "category": cat.id,
-            "image": image
-        }
-    )
-    assert (reverse("shop:index"), 302) == request.redirect_chain[0]
-    assert "Vare" in request.content.decode("utf-8")
 
 
 @pytest.mark.django_db
-def test_create_item_with_item_perms(client, create_user_item_perms, create_category, create_image):
+def test_view_create_page_item_with_item_perms(
+    client, create_user_item_perms, create_category, create_image
+):
     user = create_user_item_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-item"))
     assert request.status_code == 200
-    image = create_image
-    cat = create_category
-    request = client.post(
-        reverse("shop:add-item"),
-        follow=True,
-        data={
-            "name": "Vare",
-            "price": Decimal(10),
-            "description": "Beskrivelse",
-            "category": cat.id,
-            "image": image
-        }
-    )
-    assert (reverse("shop:index"), 302) == request.redirect_chain[0]
-    assert "Vare" in request.content.decode("utf-8")
 
 
 @pytest.mark.django_db
-def test_create_item_with_category_perms(client, create_user_category_perms):
+def test_view_create_page_item_with_category_perms(
+    client, create_user_category_perms
+):
     user = create_user_category_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-item"))
@@ -126,7 +80,9 @@ def test_create_item_with_category_perms(client, create_user_category_perms):
 
 
 @pytest.mark.django_db
-def test_create_item_with_refill_perms(client, create_user_refill_perms):
+def test_view_create_page_item_with_refill_perms(
+    client, create_user_refill_perms
+):
     user = create_user_refill_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:add-item"))
@@ -134,43 +90,19 @@ def test_create_item_with_refill_perms(client, create_user_refill_perms):
     assert "/login" in request.url
 
 
+# ----------- Refill ----------- #
 @pytest.mark.django_db
-def test_create_item_no_perms(client, create_user_no_perms):
-    user = create_user_no_perms
-    client.login(username=user.username, password="defaultpassword")
-    request = client.get(reverse("shop:add-item"))
-    assert request.status_code == 302
-    assert "/login" in request.url
-
-
-########### Refill ############
-@pytest.mark.django_db
-def test_refill_balance_with_all_perms(client, create_user_all_perms):
+def test_view_refill_balance_page_with_all_perms(client, create_user_all_perms):
     user = create_user_all_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:refill"))
     assert request.status_code == 200
-    receiver = user
-    amount = Decimal(100)
-    request = client.post(
-        reverse("shop:refill"),
-        data={
-            "receiver": receiver.id,
-            "amount": amount
-        }
-    )
-    user.profile.refresh_from_db()
-    assert request.status_code == 200
-    assert user.profile.balance == amount
-    # We have not touched any other balances
-    all_profiles = Profile.objects.all()
-    other_profiles = all_profiles.exclude(user=user)
-    for p in other_profiles:
-        assert p.balance == 0
 
 
 @pytest.mark.django_db
-def test_refill_balance_with_item_perms(client, create_user_item_perms):
+def test_view_refill_balance_page_with_item_perms(
+    client, create_user_item_perms
+):
     user = create_user_item_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:refill"))
@@ -179,7 +111,9 @@ def test_refill_balance_with_item_perms(client, create_user_item_perms):
 
 
 @pytest.mark.django_db
-def test_refill_balance_with_category_perms(client, create_user_category_perms):
+def test_view_refill_balance_page_with_category_perms(
+    client, create_user_category_perms
+):
     user = create_user_category_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:refill"))
@@ -188,34 +122,65 @@ def test_refill_balance_with_category_perms(client, create_user_category_perms):
 
 
 @pytest.mark.django_db
-def test_refill_balance_with_refill_perms(client, create_user_refill_perms):
+def test_view_refill_balance_page_with_refill_perms(
+    client, create_user_refill_perms
+):
     user = create_user_refill_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:refill"))
     assert request.status_code == 200
-    receiver = user
-    amount = Decimal(100)
-    request = client.post(
-        reverse("shop:refill"),
-        data={
-            "receiver": receiver.id,
-            "amount": amount
-        }
-    )
-    user.profile.refresh_from_db()
+
+
+# ----------- Happy Hour ----------- #
+@pytest.mark.django_db
+def test_view_refill_balance_page_with_all_perms(client, create_user_all_perms):
+    user = create_user_all_perms
+    client.login(username=user.username, password="defaultpassword")
+    request = client.get(reverse("shop:refill"))
     assert request.status_code == 200
-    assert user.profile.balance == amount
-    # We have not touched any other balances
-    all_profiles = Profile.objects.all()
-    other_profiles = all_profiles.exclude(user=user)
-    for p in other_profiles:
-        assert p.balance == 0
 
 
 @pytest.mark.django_db
-def test_refill_balance_no_perms(client, create_user_no_perms):
-    user = create_user_no_perms
+def test_view_refill_balance_page_with_item_perms(
+    client, create_user_item_perms
+):
+    user = create_user_item_perms
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("shop:refill"))
     assert request.status_code == 302
     assert "/login" in request.url
+
+
+@pytest.mark.django_db
+def test_view_refill_balance_page_with_category_perms(
+    client, create_user_category_perms
+):
+    user = create_user_category_perms
+    client.login(username=user.username, password="defaultpassword")
+    request = client.get(reverse("shop:refill"))
+    assert request.status_code == 302
+    assert "/login" in request.url
+
+
+@pytest.mark.django_db
+def test_view_refill_balance_page_with_refill_perms(
+    client, create_user_refill_perms
+):
+    user = create_user_refill_perms
+    client.login(username=user.username, password="defaultpassword")
+    request = client.get(reverse("shop:refill"))
+    assert request.status_code == 200
+
+
+# ----------- User without permissions has nothing to do in admin ----------- #
+@pytest.mark.django_db
+def test_view_any_admin_related_no_perms(client, create_user_no_perms):
+    user = create_user_no_perms
+    client.login(username=user.username, password="defaultpassword")
+    for urlpattern in urlpatterns:
+        url = urlpattern.lookup_str
+        if "admin" in url:
+            url = reverse(f"shop:{urlpattern.name}")
+            request = client.get(url)
+            assert request.status_code == 302
+            assert "/login" in request.url
