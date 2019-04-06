1b2edf2415be0a11ace99ed33dc0448f04bfa6c7
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-04-06 16:56:31 +0200

    Black formatting

--- chemie/shop/tests/factories.py
+++ chemie/shop/tests/factories.py
@@ -10,7 +10,7 @@ class CategoryFactory(factory.DjangoModelFactory):
     class Meta:
         model = Category
         # Deal with name is unique constraint
-        django_get_or_create = ('name',)
+        django_get_or_create = ("name",)
 
     name = factory.Faker("first_name")
     slug = slugify(name)
@@ -20,10 +20,9 @@ class ItemFactory(factory.DjangoModelFactory):
     class Meta:
         model = Item
         # Deal with name is unique constraint
-        django_get_or_create = ('name',)
+        django_get_or_create = ("name",)
 
     name = factory.Faker("first_name")
     price = Decimal(2)
-    description = factory.Faker("last_name")
     category = factory.SubFactory(CategoryFactory)
     image = factory.django.ImageField(color="red")
