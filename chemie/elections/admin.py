aa5d1c35364e6041b7c673298e44a02b00c64dfe
Eskild <eskild.emedd33@gmail.com>
2019-04-07 13:35:50 +0200

    Style ticket page in admin view

--- chemie/elections/admin.py
+++ chemie/elections/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 
 from .models import Candidate, Position, Election, Ticket
 
-# TODO style the position page for easier reading backend
+admin.site.site_header = "Valg"
+admin.site.site_title = "Valg"
+
+
 admin.site.register(Candidate)
 admin.site.register(Position)
 admin.site.register(Election)
-admin.site.register(Ticket)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ["get_candidates", "is_blank", "position"]
+
+    def get_candidates(self, obj):
+        return "\n".join(
+            [
+                candidate.user.get_full_name() + ","
+                for candidate in obj.candidates.all()
+            ]
+        )
