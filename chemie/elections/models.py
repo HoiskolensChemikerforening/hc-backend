017cacefb3ebe7ecc0be4f0304985cff01b811b9
Eskild <eskild.emedd33@gmail.com>
2019-04-10 10:48:39 +0200

    add spacing between lines in models.py

--- chemie/elections/models.py
+++ chemie/elections/models.py
@@ -38,9 +38,11 @@ class Candidate(models.Model):
         verbose_name="kandidatens bruker",
         on_delete=models.CASCADE,
     )
+
     votes = models.PositiveSmallIntegerField(
         verbose_name="Antall stemmer", default=0
     )
+
     pre_votes = models.PositiveSmallIntegerField(
         verbose_name="Antall forhåndstemmer", default=0
     )
@@ -59,7 +61,9 @@ class Ticket(models.Model):
         related_name="tickets",
         verbose_name="Kandidatene som stemmes på",
     )
+
     is_blank = models.BooleanField(default=False, verbose_name="Blank stemme")
+
     position = models.ForeignKey("Position", on_delete=models.CASCADE)
 
     @classmethod
@@ -79,24 +83,30 @@ class Position(models.Model):
     position_name = models.CharField(
         max_length=100, verbose_name="Navn på verv"
     )
+
     spots = models.PositiveSmallIntegerField(
         default=1, verbose_name="Antall plasser"
     )
+
     number_of_prevote_tickets = models.PositiveSmallIntegerField(
         default=0, verbose_name="Antall personer som har forhåndsstemt"
     )
+
     is_active = models.BooleanField(  # Brukere kan gå inn og stemme på denne posisjonen
         default=False, verbose_name="Er valget åpent"
     )
+
     is_done = models.BooleanField(  # valget på denne posisjon er gjennomført
         default=False, verbose_name="Valget er gjennomført"
     )
+
     candidates = models.ManyToManyField(
         Candidate,
         blank=True,
         related_name="candidate_position",
         verbose_name="Kandidater som stiller",
     )
+
     tickets = models.ManyToManyField(
         Ticket,
         blank=True,
@@ -112,6 +122,7 @@ class Position(models.Model):
         to_be_added = (
             False if user in [usr.user for usr in position_candidates] else True
         )
+
         if to_be_added:
             candidate = Candidate.objects.create(user=user)
             self.candidates.add(candidate)
@@ -120,9 +131,11 @@ class Position(models.Model):
     def delete_candidates(self, candidate_username):
         candidate_user_object = User.objects.get(username=candidate_username)
         all_candidates = self.candidates.all()
+
         try:
             candidate_object = all_candidates.get(user=candidate_user_object)
             deletable = candidate_object
+
             if deletable in self.candidates.all():
                 deletable.delete()
                 self.save()
@@ -142,9 +155,11 @@ class Position(models.Model):
     def get_blank_votes(self):
         blank_tickets = self.tickets.filter(is_blank=True)
         number_of_blank = 0
+
         for ticket in blank_tickets:
             if ticket.candidates.all().count() == 0:
                 number_of_blank += 1
+
         if (
             blank_tickets.count() != number_of_blank
         ):  # TODO: hvordan vil vi sjekke error
@@ -154,9 +169,11 @@ class Position(models.Model):
     def calculate_candidate_votes(self):  # TODO Validate this fucker with tests
         tickets = self.tickets.exclude(is_blank=True).all()
         candidates = self.candidates.all()
+
         for candidate in candidates:
             candidate.votes = 0
             candidate.save()
+
         for candidate in candidates:
             for ticket in tickets:
                 if candidate in ticket.candidates.all():
@@ -167,8 +184,10 @@ class Position(models.Model):
         tickets = self.tickets.all()
         total_votes = 0
         candidates = self.candidates.all()
+
         for candidate in candidates:
             total_votes += candidate.pre_votes
+
         for ticket in tickets:
             if ticket.is_blank:
                 total_votes += 1
@@ -180,6 +199,7 @@ class Position(models.Model):
         ticket = Ticket.create_ticket(
             candidates, self
         )  # Lager stemmeseddelen for brukeren
+
         self.tickets.add(ticket)
         user.profile.voted = True
         user.profile.save()
@@ -198,9 +218,11 @@ class Position(models.Model):
 class Election(models.Model):
     # For the entire election
     is_open = models.BooleanField(verbose_name="Er åpent", default=False)
+
     positions = models.ManyToManyField(
         Position, blank=True, related_name="election", verbose_name="Verv"
     )
+
     current_position = models.ForeignKey(
         Position,
         blank=True,
@@ -219,6 +241,7 @@ class Election(models.Model):
     def current_position_is_active(cls):
         if cls.latest_election_is_open():
             election = cls.get_latest_election()
+
             if election.current_position is not None:
                 if election.current_position.is_active:
                     return True
@@ -227,12 +250,14 @@ class Election(models.Model):
     @classmethod
     def latest_election_is_open(cls):
         """Checking if the latest election object is currently open"""
+
         try:
             election = cls.objects.latest("id")
             if election.is_open:
                 return True
         except ObjectDoesNotExist:  # TODO: more exceptions?
             pass
+
         return False
 
     @classmethod
@@ -257,13 +282,16 @@ class Election(models.Model):
         Checks if there is an active voting or the election is open,
         if not return the redirect function to the correct url
         """
+
         if not cls.latest_election_is_open():
             return True, redirect("elections:admin_start_election")
         else:
             election = cls.get_latest_election()
+
             if election.current_position_is_active():
                 pk = election.current_position.id
                 return True, redirect("elections:voting_active", pk=pk)
+
             return False, None
 
     def add_position(self, position_name, spots):
@@ -281,8 +309,10 @@ class Election(models.Model):
 
     def delete_position(self, position_id):
         position = self.positions.get(id=int(position_id))
+
         for candidate in position.candidates.all():
             position.delete_candidates(candidate_username=candidate.user.username)
+
         position.delete()
         self.save()
         return
@@ -290,11 +320,13 @@ class Election(models.Model):
     def start_current_position_voting(self, position):
         # Find candidates running for current position
         candidates = position.candidates.all()
+
         if len(candidates) <= 0:
             return False
         else:
             Profile.objects.filter(voted=True).update(voted=False)
             # forhindrer at vi ikke resetter votes ved refresh page
+
             self.current_position = position
             self.current_position.is_active = True
             self.current_position.save()
@@ -316,9 +348,11 @@ class Election(models.Model):
 
     def end_election(self):
         positions = self.positions.all()
+
         for position in positions:
             position.is_active = False
             position.save()
+
         self.current_position = None
         self.is_open = False
         self.save()
