ac20a166fa36b281561fc5880b1adc904ef8fc98
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-04-15 22:07:51 +0200

    Norwegian urls and change from url to path

--- chemie/shop/urls.py
+++ chemie/shop/urls.py
@@ -1,28 +1,28 @@
-from django.conf.urls import url
-from . import views
+from django.urls import path
 
+from . import views
 
 app_name = "shop"
 
 urlpatterns = [
-    url(r"^$", views.index, name="index"),
-    url(r"^mine-kvitteringer$", views.view_my_receipts, name="receipts"),
-    url(r"^admin/$", views.admin, name="admin"),
-    url(r"^admin/refill-balance", views.refill, name="refill"),
-    url(r"^admin/add-item/$", views.add_item, name="add-item"),
-    url(
-        r"^admin/add-item/edit/(?P<pk>[0-9]+)",
+    path("", views.index, name="index"),
+    path("mine-kvitteringer", views.view_my_receipts, name="receipts"),
+    path("admin/", views.admin, name="admin"),
+    path("admin/fyll-konto", views.refill, name="refill"),
+    path("admin/opprett-vare/", views.add_item, name="add-item"),
+    path(
+        "admin/opprett-vare/endre/<int:pk>)",
         views.edit_item,
         name="edit-item",
     ),
-    url(r"^admin/add-category/", views.add_category, name="add-category"),
-    url(
-        r"^admin/remove-item/(?P<pk>[0-9]+)",
+    path("admin/opprett-kategori/", views.add_category, name="add-category"),
+    path(
+        "admin/fjern-vare/<int:pk>)",
         views.remove_item,
         name="remove-item",
     ),
-    url(
-        r"^admin/activate-happyhour/",
+    path(
+        "admin/aktiver-happyhour/",
         views.activate_happyhour,
         name="activate-happyhour",
     ),
