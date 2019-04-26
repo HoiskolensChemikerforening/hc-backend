9398b423909e9de87dd7fd0ec13dcb1728385df5
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-04-26 22:39:49 +0200

    Make all tests pass with new Election systems

--- chemie/elections/tests/test_views.py
+++ chemie/elections/tests/test_views.py
@@ -1,10 +1,13 @@
 import pytest
 from django.shortcuts import reverse
 
+from chemie.elections.models import Ticket
+
 
 @pytest.mark.django_db
 def test_user_not_logged_in(client):
-    request = client.get(reverse("elections:vote"))
+    # Try to access front page
+    request = client.get(reverse("elections:index"))
     assert request.status_code == 302
     assert reverse("login") in request.url
 
@@ -14,70 +17,109 @@ def test_election_not_open(client, create_user):
     user = create_user
     client.login(username=user.username, password="defaultpassword")
 
-    request = client.get(reverse("elections:vote"))
+    request = client.get(reverse("elections:index"))
     assert "Valget har ikke åpnet enda" in request.content.decode("utf-8")
     assert (
-        '<a class="button" href="/elections/results">'
-        in request.content.decode("utf-8")
+            "Resultater fra forrige valg"
+            in request.content.decode("utf-8")
     )
+
+    request = client.get(reverse("elections:vote"))
+    assert request.status_code == 302
+    assert request.url == reverse("elections:index")
 
     request = client.get(reverse("elections:has_voted"))
     assert request.status_code == 302
-    assert reverse("elections:vote") == request.url
-    request = client.get(reverse("elections:voting"))
-    assert request.status_code == 302
-    assert reverse("elections:vote") == request.url
+    assert request.url == reverse("elections:index")
 
 
 @pytest.mark.django_db
-def test_election_is_open(client, create_user, create_election_with_positions):
+def test_election_is_open_no_position_open(client, create_user, create_election_with_positions):
     election, positions = create_election_with_positions
     user = create_user
     client.login(username=user.username, password="defaultpassword")
-    request = client.get(reverse("elections:voting"))
+    # Access the vote page. Get redirected to index
+    request = client.get(reverse("elections:vote"))
     assert request.status_code == 302
-    assert request.url == reverse("elections:vote")
+    assert request.url == reverse("elections:index")
 
     election.is_open = True
     election.save()
 
     client.login(username=user.username, password="defaultpassword")
     request = client.get(reverse("elections:vote"))
-    assert request.status_code is 200
-    assert "Klargjøres til valg" in request.content.decode("utf-8")
+    assert request.status_code == 302
+    assert request.url == reverse("elections:index")
 
     request = client.get(reverse("elections:has_voted"))
     assert request.status_code == 302
-    assert reverse("elections:vote") == request.url
-    request = client.get(reverse("elections:voting"))
-    assert request.status_code == 302
-    assert reverse("elections:vote") == request.url
+    assert request.url == reverse("elections:index")
 
 
 @pytest.mark.django_db
-def test_get_vote_page(
-    client, create_user, create_open_election_with_position_and_candidates
+def test_open_for_voting_with_and_without_checkin(
+        client, create_user, create_open_election_with_position_and_candidates
 ):
     election = create_open_election_with_position_and_candidates
     position = election.positions.first()
-    election.start_current_election(position)
+    election.start_current_position_voting(position.id)
+    election.save()
+
+    user = create_user
+    client.login(username=user.username, password="defaultpassword")
+
+    # User not checked in
+    request = client.get(reverse("elections:index"))
+    assert request.status_code == 200
+    assert "Du har ikke sjekket inn" in request.content.decode("utf-8")
+
+    request = client.get(reverse("elections:vote"))
+    assert request.status_code == 302
+    assert request.url == reverse("elections:index")
+
+    # User has checked in
+    user.profile.eligible_for_voting = True
+    user.profile.save()
+    request = client.get(reverse("elections:index"))
+    assert request.status_code == 200
+    assert "Gå til avstemning" in request.content.decode("utf-8")
+
+    request = client.get(reverse("elections:vote"))
+    assert request.status_code == 200
+    assert "Stem" in request.content.decode("utf-8")
+
+
+# client.login(username=user.username, password="defaultpassword")
+# request = client.get(reverse("elections:vote"))
+# assert request.status_code is 200
+# assert "Klargjøres til valg" in request.content.decode("utf-8")
+
+@pytest.mark.django_db
+def test_get_vote_page(
+        client, create_user, create_open_election_with_position_and_candidates
+):
+    election = create_open_election_with_position_and_candidates
+    position = election.positions.first()
+    election.start_current_position_voting(position.id)
     election.save()
     election.refresh_from_db()
 
     user = create_user
+    user.profile.eligible_for_voting = True
+    user.profile.save()
     client.login(username=user.username, password="defaultpassword")
 
     # Check that when user accesses url where he has voted, he is redirected to vote page
     request = client.get(reverse("elections:has_voted"), follow=True)
     assert request.status_code == 200
-    assert (reverse("elections:voting"), 302) == request.redirect_chain[0]
+    assert (reverse("elections:vote"), 302) == request.redirect_chain[0]
     assert election.current_position.position_name in request.content.decode(
         "utf-8"
     )
 
     # Let user access vote page
-    request = client.get(reverse("elections:voting"))
-    assert request.status_code is 200
+    request = client.get(reverse("elections:vote"))
+    assert request.status_code == 200
     assert position.position_name in request.content.decode("utf-8")
     assert str(position.spots) in request.content.decode("utf-8")
     for candidate in position.candidates.all():
@@ -86,15 +128,15 @@ def test_get_vote_page(
 
 @pytest.mark.django_db
 def test_vote_for_one_user(
-    client, create_user, create_election_with_positions, create_candidates
+        client, create_user, create_election_with_positions, create_candidates
 ):
     election, positions = create_election_with_positions
     candidates = create_candidates
     election.is_open = True
     candidate, position = candidates[0], positions[0]
     position.candidates.add(*candidates)
-    election.add_position(position)
-    election.start_current_election(position)
+    election.positions.add(position)
+    election.start_current_position_voting(position.id)
     election.save()
     election.refresh_from_db()
 
@@ -104,49 +146,60 @@ def test_vote_for_one_user(
         assert cand in election_candidates
 
     user = create_user
+    user.profile.eligible_for_voting = True
+    user.profile.save()
 
+    # Vote for the candidate chosen
     client.login(username=user.username, password="defaultpassword")
     request = client.post(
-        reverse("elections:voting"),
-        {"Stem": "Avgi stemme", "candidates": candidate.id},
+        reverse("elections:vote"),
+        {"candidates": candidate.id},
     )
     assert request.url == reverse("elections:has_voted")
-    candidate.refresh_from_db()
-    assert candidate.votes == 1
-    for cand in candidates[1:]:
-        cand.refresh_from_db()
-        assert cand.votes == 0
-    user.profile.refresh_from_db()
-    assert user.profile.voted is True
-
-    # Check that when user accesses url where he has voted, he receives proper message
-    request = client.get(reverse("elections:has_voted"))
-    assert "Du har stemt!" in request.content.decode("utf-8")
 
     # Check that when user tries to vote again, he is redirected
     request = client.post(
         reverse("elections:vote"), {"candidates": candidate.id}, follow=True
     )
-    assert "Du har alt stemt" in request.content.decode("utf-8")
+    assert (reverse("elections:has_voted"), 302) == request.redirect_chain[0]
+    assert "Din stemme har blitt registrert" in request.content.decode("utf-8")
 
-    # Close election and check the results
-    position.end_voting_for_position()
-    election.end_election()
-    request = client.get(reverse("elections:results"))
-    assert candidate.user.get_full_name() in request.content.decode("utf-8")
+    # Check that when user accesses url where he has voted, he receives proper message
+    request = client.get(reverse("elections:has_voted"))
+    assert request.status_code == 200
+    assert "Din stemme har blitt registrert" in request.content.decode("utf-8")
+
+    # Check that only one vote ticket was submitted
+    assert Ticket.objects.all().count() == 1
+
+    # Close voting and assign votes to candidates by counting tickets
+    election.end_current_position_voting()
+    position.calculate_candidate_votes()
+
+    # Make sure no blank votes were received
+    assert position.get_blank_votes() == position.get_blank_votes()
+    candidate.refresh_from_db()
+    assert candidate.votes == 1
+    assert candidate.pre_votes == 0
+    for cand in candidates[1:]:
+        cand.refresh_from_db()
+        assert cand.votes == 0
+        assert cand.pre_votes == 0
+    user.profile.refresh_from_db()
+    assert user.profile.voted is True
 
 
 @pytest.mark.django_db
 def test_vote_for_multiple_users(
-    client, create_user, create_election_with_positions, create_candidates
+        client, create_user, create_election_with_positions, create_candidates
 ):
     election, positions = create_election_with_positions
     candidates = create_candidates
     election.is_open = True
     candidate, position = candidates[0], positions[0]
     position.candidates.add(*candidates)
-    election.add_position(position)
-    election.start_current_election(position)
+    election.positions.add(position)
+    election.start_current_position_voting(position.id)
     election.save()
     election.refresh_from_db()
 
@@ -156,75 +209,102 @@ def test_vote_for_multiple_users(
         winner_candidates.append(candidates[i])
 
     user = create_user
+    user.profile.eligible_for_voting = True
+    user.profile.save()
     client.login(username=user.username, password="defaultpassword")
     client.post(
-        reverse("elections:voting"),
+        reverse("elections:vote"),
         {
-            "Stem": "Avgi stemme",
             "candidates": [cand.id for cand in winner_candidates],
         },
     )
+    # Close voting and assign votes to candidates by counting tickets
+    election.end_current_position_voting()
+    position.calculate_candidate_votes()
+
+    # Count votes
+    assert position.get_blank_votes() == 0
     for cand in winner_candidates:
         cand.refresh_from_db()
-        assert cand.votes is 1
-    for cand in candidates[number_of_winners + 1 :]:
+        assert cand.votes == 1
+        assert cand.pre_votes == 0
+    for cand in candidates[number_of_winners + 1:]:
         cand.refresh_from_db()
-        assert cand.votes is 0
+        assert cand.votes == 0
     position.refresh_from_db()
-    assert position.total_votes is number_of_winners
+    assert position.get_total_votes() == number_of_winners
     user.profile.refresh_from_db()
     assert user.profile.voted is True
 
 
 @pytest.mark.django_db
 def test_vote_blank(
-    client, create_user, create_election_with_positions, create_candidates
+        client, create_user, create_election_with_positions, create_candidates
 ):
     election, positions = create_election_with_positions
     candidates = create_candidates
     election.is_open = True
     position = positions[0]
     position.candidates.add(*candidates)
-    election.add_position(position)
-    election.start_current_election(position)
+    election.positions.add(position)
+    election.start_current_position_voting(position.id)
     election.save()
     election.refresh_from_db()
 
     user = create_user
+    user.profile.eligible_for_voting = True
+    user.profile.save()
     client.login(username=user.username, password="defaultpassword")
-    client.post(reverse("elections:voting"), {"Blank": "Stem blankt"})
+    client.post(reverse("elections:vote"), {"candidates": []})
+
+    # Close voting and assign votes to candidates by counting tickets
+    election.end_current_position_voting()
+    position.calculate_candidate_votes()
+
+    # Count votes
     for cand in candidates:
         cand.refresh_from_db()
-        assert cand.votes is 0
+        assert cand.votes == 0
     position.refresh_from_db()
-    assert position.total_votes is 1
+    assert position.get_total_votes() == 1
+    assert position.get_blank_votes() == 1
     user.profile.refresh_from_db()
     assert user.profile.voted is True
 
 
 @pytest.mark.django_db
 def test_invalid_vote(
-    client, create_user, create_election_with_positions, create_candidates
+        client, create_user, create_election_with_positions, create_candidates
 ):
     election, positions = create_election_with_positions
     candidates = create_candidates
     election.is_open = True
     position = positions[0]
     position.candidates.add(*candidates)
-    election.add_position(position)
-    election.start_current_election(position)
+    election.positions.add(position)
+    election.start_current_position_voting(position.id)
     election.save()
     election.refresh_from_db()
 
     user = create_user
+    user.profile.eligible_for_voting = True
+    user.profile.save()
     client.login(username=user.username, password="defaultpassword")
     # Trying to vote for all candidates, which are more candidates than position spots
     request = client.post(
-        reverse("elections:voting"),
+        reverse("elections:vote"),
         {"candidates": [cand.id for cand in candidates]},
     )
-    assert "Du stemte på {} kandidater, og det skal velges {} kandidater".format(
-        len(candidates), position.spots
-    ) in request.content.decode(
-        "utf-8"
-    )
+    # Submitting a vote containing too many candidates is checked with Javascript
+    # The user is therefore redirected
+    assert request.status_code == 302
+    assert request.url == reverse("elections:has_voted")
+
+    # Close voting and assign votes to candidates by counting tickets
+    election.end_current_position_voting()
+    position.calculate_candidate_votes()
+    assert Ticket.objects.all().count() == 0
+    for candidate in candidates:
+        candidate.refresh_from_db()
+        assert candidate.pre_votes == 0
+    assert user.profile.voted is False
