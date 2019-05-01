19814bd8d347bcfc1986ae4a3cb47424aafae588
Eskild <eskild.emedd33@gmail.com>
2019-05-01 09:08:36 +0200

    split up url pattern into admin and shop pattern

--- chemie/shop/urls.py
+++ chemie/shop/urls.py
@@ -4,23 +4,29 @@ from . import views
 
 app_name = "shop"
 
-urlpatterns = [
+shop_url_patterns = [
     path("", views.index, name="index"),
     path("mine-kvitteringer", views.view_my_receipts, name="receipts"),
     path("mine-paafyll", views.view_my_refills, name="refills"),
     path("statistikk", views.view_statistics, name="statistics"),
+    path("fjern-handlekurv/", views.remove_cart, name="remove-cart"),
+]
+
+admin_shop_urlpatterns = [
     path("admin/", views.admin, name="admin"),
-    path("admin/fyll-konto", views.refill, name="refill"),
+    path("admin/opprett-kategori/", views.add_category, name="add-category"),
     path("admin/opprett-vare/", views.add_item, name="add-item"),
     path(
         "admin/opprett-vare/endre/<int:pk>", views.edit_item, name="edit-item"
     ),
-    path("admin/opprett-kategori/", views.add_category, name="add-category"),
+    path("admin/fyll-konto", views.refill, name="refill"),
     path("fjern-vare/<int:pk>", views.remove_item, name="remove-item"),
-    path("fjern-handlekurv/", views.remove_cart, name="remove-cart"),
+
     path(
         "admin/aktiver-happyhour/",
         views.activate_happyhour,
         name="activate-happyhour",
     ),
 ]
+
+urlpatterns = shop_url_patterns + admin_shop_urlpatterns
