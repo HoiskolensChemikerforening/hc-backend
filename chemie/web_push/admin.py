c05c6546250d26e81a5f93ed4117a555ab319508
Eskild <eskild.emedd33@gmail.com>
2019-05-09 10:13:50 +0200

    small fixes and remova of unnecessary code

--- chemie/web_push/admin.py
+++ chemie/web_push/admin.py
@@ -1,20 +1,53 @@
 from django.contrib import admin
-from .models import Device,CoffeeSubmission
+from .models import Device, CoffeeSubmission
 from django.contrib import admin
-from push_notifications.models import APNSDevice, GCMDevice, WebPushDevice, WNSDevice
+from push_notifications.models import (
+    APNSDevice,
+    GCMDevice,
+    WebPushDevice,
+    WNSDevice,
+)
 from push_notifications.admin import DeviceAdmin, GCMDeviceAdmin
 
 
 # TODO fix deletion of device and gcm device
 # TODO style admin page
 # TODO remove gcm_device/apns_device from admin page
+
+
+def delete_inactive_devices(modeladmin, request, queryset):
+    inactive_gcm_devices = queryset.filter(gcm_device__active=False)
+    inactive_gcm_devices.delete()
+
+    # inactive_apns_devices = queryset.filter(apns_device__active=False)
+    # inactive_apns_devices.delete()
+
+
+delete_inactive_devices.short_description = "Delete inactive devices"
+
+
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('gcm_device','coffee_subscription', 'news_subscription','date_created')
+    list_display = (
+        "owner",
+        "coffee_subscription",
+        "date_created",
+        "is_active",
+        "Device_token",
+    )
+    actions = [delete_inactive_devices]
+
+    def Device_token(cls, obj):
+        return obj.gcm_device.registration_id
+
+    def Is_active(cls, obj):
+        return obj.gcm_device.active
+
 
 @admin.register(CoffeeSubmission)
 class CoffeeAdmin(admin.ModelAdmin):
-    list_display = ('date',)
+    list_display = ("date",)
+
 
 admin.site.unregister(APNSDevice)
 admin.site.unregister(GCMDevice)
