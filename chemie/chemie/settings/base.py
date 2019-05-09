06d4c137a5089a7f9456a47e2e380b4cbe5e40ee
Eskild <eskild.emedd33@gmail.com>
2019-05-09 10:07:39 +0200

    remove unecessary restful settings

--- chemie/chemie/settings/base.py
+++ chemie/chemie/settings/base.py
@@ -245,8 +245,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
 }
 
 # FIREBASE PUSH NOTIFICATION CONFIGURATION
