9cbc0c51889966c8dbec72247460311067d12839
Eskild <eskild.emedd33@gmail.com>
2019-05-15 13:03:54 +0200

    remove admin delete inactive devices method

--- chemie/web_push/admin.py
+++ chemie/web_push/admin.py
@@ -19,7 +19,7 @@ class WebPushDeviceAdmin(admin.ModelAdmin):
         "is_active",
         "Device_token",
     )
-    actions = ["delete_inactive_devices", "send_message"]
+    actions = ["send_message"]
 
     def Device_token(cls, obj):
         if obj.gcm_device is not None:
@@ -84,35 +84,6 @@ class WebPushDeviceAdmin(admin.ModelAdmin):
 
     def send_message(self, request, queryset):
         self.send_messages(request, queryset)
-
-    def delete_inactive_devices(self, request, queryset):
-
-        unregistered_gcm_devices = queryset.filter(gcm_device=None)
-        """
-        The commented lines below is the implementation for Safari browser
-        Apples APNS certificat would be needed, cost ~1000 NOK/year
-        The code has not been testet so no garanties it would work
-        """
-        # unregistered_apns_devices = unregistered_gcm_devices.exclude(apns_device__isnull=True)
-        # unregistered_device = unregistered_gcm_devices.filter(apns_device__isnull=True)
-        # unregistered_device.delete()
-        # unregistered_apns_devices.delte()
-        unregistered_gcm_devices.delete()
-
-        inactive_gcm_devices = queryset.filter(gcm_device__active=False)
-        inactive_gcm_devices.delete()
-
-        """
-        The commented lines below is the implementation for Safari browser
-        Apples APNS certificat would be needed, cost ~1000 NOK/year
-        The code has not been testet so no garanties it would work
-        """
-
-        # inactive_apns_devices = queryset.filter(apns_device__active=False)
-        # inactive_apns_devices.delete()
-
-    delete_inactive_devices.short_description = "Slett inactive devices"
-    send_message.short_description = "Send test melding"
 
 @admin.register(CoffeeSubmission)
 class CoffeeAdmin(admin.ModelAdmin):
