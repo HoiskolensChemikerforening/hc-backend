59a422de4a3ca4df3b760e9a7190b8cf906354da
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2019-12-31 15:31:45 +0100

    Cached pip package installations so that not everything must be downloaded and reinstalled in docker if changes to requirements are made

--- chemie/entrypoint.sh
+++ chemie/entrypoint.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 printenv > /etc/environment
 cd /
-sleep 10
+# Wait for Postgres to start
+echo "Waiting for postgres..."
+while ! nc -z database $PGPORT; do
+    # While database connection not open: wait
+    sleep 0.5
+done
+echo "PostgreSQL started"
+
 python manage.py migrate
 if [ "$DEBUG" == "False" ]
 then
   python manage.py collectstatic --noinput
 fi
 
-# Cronmanager uwsgi
+# Cronmanager uwsgi: starts conjobs
 uwsgi -d /dev/null --chdir=./ --module=chemie.chemie.cron_wsgi:application --env DJANGO_SETTINGS_MODULE=chemie.chemie.settings.production \
 --master --pidfile=/tmp/project-master-cron.pid --http=0.0.0.0:8001 --processes=5 \
 --harakiri=20 --max-requests=5000 --vacuum
 
-# Django main uwsgi
+# Django main uwsgi: starts website
 uwsgi --chdir=./ --module=chemie.chemie.wsgi:application --env DJANGO_SETTINGS_MODULE=chemie.chemie.settings.production \
 --master --pidfile=/tmp/project-master.pid --http=0.0.0.0:8000 --processes=5 \
 --harakiri=20 --max-requests=5000 --vacuum
