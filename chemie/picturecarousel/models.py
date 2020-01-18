e1fb0c41e650bc5259a4b783fbf8a384740a7c2e
bendissa <bendik.sannes@gmail.com>
2020-01-18 23:16:47 +0100

    Add ManyToMany relation between pictures and users

--- chemie/picturecarousel/models.py
+++ chemie/picturecarousel/models.py
@@ -16,6 +16,7 @@ class Contribution(models.Model):
     approved = models.BooleanField(default=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+    tagged_users = models.ManyToManyField(User, blank=True, related_name="tagged_pictures")
 
     objects = ContributionManager()
 
