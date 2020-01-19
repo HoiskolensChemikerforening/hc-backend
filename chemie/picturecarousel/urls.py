f3c804ae653e1c310f871762daeec3dd8f7a5d97
bendissa <bendik.sannes@gmail.com>
2020-01-19 19:36:29 +0100

    Change url() to path() in dispatcher and norwegify urls

--- chemie/picturecarousel/urls.py
+++ chemie/picturecarousel/urls.py
@@ -6,26 +6,24 @@ from . import views
 app_name = "picturecarousel"
 
 urlpatterns = [
-    url(r"^send/", views.submit_picture, name="submit"),
-    url(r"^$", views.view_carousel, name="display"),
-    path("overview/", views.approve_pictures, name="overview"),
+    path("", views.submit_picture, name="submit"),
+    path("karusell", views.view_carousel, name="display"),
+    path("godkjenn/", views.approve_pictures, name="overview"),
     path(
-        "overview/<int:page>", views.approve_pictures, name="overview_detail"
+        "godkjenn/<int:page>", views.approve_pictures, name="overview_detail"
     ),
-    url(
-        r"^overview/approve/(?P<picture_id>[0-9]+)",
-        views.approve_deny,
-        name="approve",
+    path(
+        "godkjenn/approve/<int:picture_id>", views.approve_deny, name="approve"
     ),
-    url(
-        r"^overview/deny/(?P<picture_id>[0-9]+)",
+    path(
+        "godkjenn/deny/<int:picture_id>",
         views.approve_deny,
         {"deny": True},
         name="deny",
     ),
-    path("bilder/", views.view_pictures, name="view_pictures"),
+    path("aktive/", views.view_pictures, name="view_pictures"),
     path(
-        "bilder/<int:page>", views.view_pictures, name="view_pictures_detail"
+        "aktive/<int:page>", views.view_pictures, name="view_pictures_detail"
     ),
-    path("tag/<int:id>", views.tag_users, name="tag_users"),
+    path("tagg/<int:id>", views.tag_users, name="tag_users"),
 ]
