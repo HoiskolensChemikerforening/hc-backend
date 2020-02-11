cfe112b0f394e2ebc1cabca4ec76f014fad20210
bendikstoasannes <bendik.sannes@gmail.com>
2020-02-11 12:52:55 +0100

    Formatting. Dette kan ta en stund å bli vant til

--- chemie/customprofile/urls.py
+++ chemie/customprofile/urls.py
@@ -8,11 +8,21 @@ urlpatterns = [
     path("profil/registrer/", views.register_user, name="register"),
     path("profil/rediger/", views.edit_profile, name="edit"),
     path("profil/rediger-push-varsler/", views.edit_push, name="edit-push"),
-    path("profil/glemt-passord/", views.forgot_password, name="forgotpassword"),
-    re_path(r"^profil/aktiver/(?P<code>[a-z0-9]{32})/?", views.activate_password, name="activate"),
-    path("profil/medlemmer/endre/<int:profile_id>/", views.change_membership_status, name="membership"),
+    path(
+        "profil/glemt-passord/", views.forgot_password, name="forgotpassword"
+    ),
+    re_path(
+        r"^profil/aktiver/(?P<code>[a-z0-9]{32})/?",
+        views.activate_password,
+        name="activate",
+    ),
+    path(
+        "profil/medlemmer/endre/<int:profile_id>/",
+        views.change_membership_status,
+        name="membership",
+    ),
     path("profil/medlemmer/", views.view_memberships, name="memberships"),
     path("profil/studentkort/", views.add_rfid, name="add_rfid"),
     path("katalog/<int:year>/", views.yearbook, name="yearbook-grade"),
-    path("katalog/", views.yearbook, name="yearbook-index")
+    path("katalog/", views.yearbook, name="yearbook-index"),
 ]
