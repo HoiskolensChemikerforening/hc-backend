06e3621a2eb6340cc922a30a0bfed659237141db
bendikstoasannes <bendik.sannes@gmail.com>
2020-02-17 19:25:11 +0100

    Update urls for shitbox and yearbook app

--- chemie/shitbox/urls.py
+++ chemie/shitbox/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
-from .views import submissions_overview, post_votes
+from django.urls import path
+from .views import *
 
 app_name = "shitbox"
 
 urlpatterns = [
-    url(r"list/$", submissions_overview, name="list"),
-    url(r"list/(?P<page>[0-9]*)$", submissions_overview, name="list-page"),
-    url(r"^$", post_votes, name="index"),
+    path("liste/", submissions_overview, name="list"),
+    path("liste/<int:page>/", submissions_overview, name="list-page"),
+    path("", post_votes, name="index"),
 ]
