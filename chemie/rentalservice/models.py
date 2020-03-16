c8bad3148fb8f32c0267a7dd92a96c733262b7dd
Helene Bøe <helene.boe@ntebb.no>
2020-03-16 18:55:42 +0100

    Edited models.py to show name

--- chemie/rentalservice/models.py
+++ chemie/rentalservice/models.py
@@ -7,3 +7,6 @@ class RentalObject(models.Model):
     name = models.CharField(max_length=100)
     description = RichTextField(verbose_name="Beskrivelse", config_name="news")
     image = ImageField(upload_to="rentalservice", verbose_name="Bilde")
+
+    def __str__(self):
+        return self.name
