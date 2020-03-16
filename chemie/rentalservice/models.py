ad2c20af5728371ecb041f5c1fbb8601e5a07ccc
Yoonsik Oh <yoonsikga@gmail.com>
2020-03-16 18:46:16 +0100

    Changes to the models.py file

--- chemie/rentalservice/models.py
+++ chemie/rentalservice/models.py
@@ -5,5 +5,5 @@ from sorl.thumbnail import ImageField
 
 class RentalObject(models.Model):
     name = models.CharField(max_length=100)
-    description = RichTextField(verbose_name="Beskrivelse")
+    description = RichTextField(verbose_name="Beskrivelse", config_name="news")
     image = ImageField(upload_to="rentalservice", verbose_name="Bilde")
