68983773f4e69e44c04f2016168b68490624a0cb
Helene Bøe <helene.boe@ntebb.no>
2020-03-18 22:58:50 +0100

    created index page and context in views

--- chemie/rentalservice/views.py
+++ chemie/rentalservice/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import RentalObject
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Heihei velkommen")
+    rentalObjects = RentalObject.objects.all().order_by("name")
+
+    context={
+        "rentalObjects" : rentalObjects
+    }
+    return render(request, "rentalservice/index.html", context)
