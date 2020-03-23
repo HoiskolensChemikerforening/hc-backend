261407f5c199e4f101a8de3b99468605d0eee6e8
Axel Andreassen <axeandre74@gmail.com>
2020-03-23 18:08:49 +0100

    created class landlord

--- chemie/rentalservice/models.py
+++ chemie/rentalservice/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 from sorl.thumbnail import ImageField
+from chemie.committees.models import Committee
+
+
+class Landlord(models.Model):  # Utleier aka promokom/ac
+    committee = models.ForeignKey(Committee, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.committee
 
 
 class RentalObject(models.Model):
     name = models.CharField(max_length=100)
     description = RichTextField(verbose_name="Beskrivelse", config_name="news")
     image = ImageField(upload_to="rentalservice", verbose_name="Bilde")
+    owner = models.ForeignKey(Landlord, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
