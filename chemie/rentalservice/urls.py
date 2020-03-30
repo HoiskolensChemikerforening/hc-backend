cb4b2591dc49788835beaa9df14b66d2fdf358f3
Yoonsik Oh <yoonsikga@gmail.com>
2020-03-30 18:33:17 +0200

    Made new view for creating new rentalobjects

--- chemie/rentalservice/urls.py
+++ chemie/rentalservice/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 app_name = "rentalservice"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [path("", views.index, name="index"),
+               path("ny", views.new_object, name="new_object")]
+
--- chemie/rentalservice/views.py
+++ chemie/rentalservice/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import RentalObject
 from django.http import HttpResponse
+from .forms import RentalObjectForm
 
 
 def index(request):
     rentalObjects = RentalObject.objects.all().order_by("name")
 
-    context={
-        "rentalObjects" : rentalObjects
-    }
+    context = {"rentalObjects": rentalObjects}
     return render(request, "rentalservice/index.html", context)
+
+
+def new_object(request):
+    form = RentalObjectForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("rentalservice:index")
+
+    context = {"form": form}
+    return render(request, "rentalservice/new_object.html", context)
