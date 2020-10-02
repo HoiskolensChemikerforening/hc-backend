56cb192a779a7a18baf176b3ca8e08dd01016dbd
Helene Boee <helebo@stud.ntnu.no>
2020-10-02 16:10:34 +0200

    Create better quiz score display in admin, set API url path.

--- chemie/quiz/urls.py
+++ chemie/quiz/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 app_name = "quiz"
 
 urlpatterns = [
-
     path("", views.index, name="index"),
+    path("api/", views.ListAllQuizTerms.as_view()),
+    path("api/", views.ListAllScores.as_view()),
+    path("api/", views.QuizTermDetail.as_view()),
+    path("api/", views.QuizScoreDetail.as_view()),
     path("navnequiz", views.name_quiz_index, name="name_quiz_index"),
     path("navnequiz/<int:year>", views.name_quiz, name="name_quiz"),
     path("kjellerquiz", views.kjellerquiz_index, name="kjellerquiz_index"),
