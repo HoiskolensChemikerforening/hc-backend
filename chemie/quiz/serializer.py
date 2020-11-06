12dd059164f5af997a8abc31557c514d82c23871
Helene Boee <helebo@stud.ntnu.no>
2020-11-06 23:37:56 +0100

    Include UserSerializer in serializers for quiz.

--- chemie/quiz/serializer.py
+++ chemie/quiz/serializer.py
@@ -1,18 +1,6 @@
 from rest_framework import serializers
 from .models import QuizTerm, QuizScore
-from django.contrib.auth.models import User
-
-# from chemie.customprofile.serializers import UserSerializer
-
-
-class UserSerializer(serializers.ModelSerializer):
-
-    full_name = serializers.CharField(source="get_full_name")
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "first_name", "last_name", "full_name")
-
+from chemie.customprofile.serializers import UserSerializer
 
 class QuizTermSerializer(serializers.ModelSerializer):
 
