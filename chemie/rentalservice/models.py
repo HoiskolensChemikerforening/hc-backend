378f1a0314c1153067fa6e7e8a98b35196d11fda
yoonsiko <yoonsiko@stud.ntnu.no>
2021-02-20 12:44:13 +0100

    Update RentalObject

--- chemie/rentalservice/models.py
+++ chemie/rentalservice/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-
 from ckeditor.fields import RichTextField
 from sorl.thumbnail import ImageField
 from chemie.committees.models import Committee
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Landlord(models.Model):  # Utleier aka promokom/ac
@@ -20,6 +20,12 @@ class RentalObject(models.Model):
     owner = models.ForeignKey(
         Landlord, null=True, blank=True, on_delete=models.CASCADE
     )
+    price = models.FloatField(
+        validators=[MinValueValidator(0.0)], null=True, blank=True
+    )  # Trenger min og maks verdi
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0)], null=True, blank=True
+    )  # Trenger min og maks verdi
 
     def __str__(self):
         return self.name
