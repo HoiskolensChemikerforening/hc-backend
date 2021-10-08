0ccb9f42a766e18b66ecef9c1a2ec32865cc1fce
yoonsiko <yoonsiko@stud.ntnu.no>
2021-10-08 18:07:32 +0200

    create refund form app

---
+++ chemie/refund/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RefundConfig(AppConfig):
+    name = 'refund'
---
+++ chemie/refund/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from sorl.thumbnail import ImageField
+
+
+class Refund(models.Model):
+    date = models.DateField(verbose_name="Utleggsdato")
+    store = models.CharField(verbose_name="Kjøpssted")
+    item = models.CharField(verbose_name="Vare")
+    event = models.CharField(verbose_name="Event")
+    price = models.FloatField(verbose_name="Pris")
+
+    def __str__(self):
+        return str(self.date) + str(self.item)
+
+
+class RefundForm(models.Model):
+    refunds = models.ForeignKey(
+        Refund, related_name="refusjon", on_delete=models.CASCADE
+    )
+    date = models.DateField(verbose_name="Dato")
+    name = models.CharField(max_length=50, verbose_name="Navn")
+    receipt = ImageField(upload_to="refund", verbose_name="Kvittering")
+    account = models.IntegerField(verbose_name="Kontonummer")
+
+    def __str__(self):
+        return str(self.date)+str(self.name)
