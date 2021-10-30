abf92c0329f4addce866738c62744fd9efb98ac6
Håkon Pelsholen Busterud <haakonbusterud@gmail.com>
2021-10-30 14:17:05 +0200

    Fix inconsistencies with migrations (#384)

---
+++ chemie/rentalservice/migrations/0001_initial.py
@@ -0,0 +1,71 @@
+import ckeditor.fields
+from django.db import migrations, models
+import django.db.models.deletion
+import sorl.thumbnail.fields
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ("committees", "0003_auto_20180127_0949"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="Landlord",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "committee",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="committees.Committee",
+                    ),
+                ),
+            ],
+        ),
+        migrations.CreateModel(
+            name="RentalObject",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                (
+                    "description",
+                    ckeditor.fields.RichTextField(verbose_name="Beskrivelse"),
+                ),
+                (
+                    "image",
+                    sorl.thumbnail.fields.ImageField(
+                        upload_to="rentalservice", verbose_name="Bilde"
+                    ),
+                ),
+                (
+                    "owner",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="rentalservice.Landlord",
+                    ),
+                ),
+            ],
+        ),
+    ]
