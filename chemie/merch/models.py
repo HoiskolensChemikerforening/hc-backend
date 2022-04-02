1f02963bf576ed37c460aeb8dc013740e7543c8c
yoonsiko <yoonsiko@stud.ntnu.no>
2022-04-02 12:24:19 +0200

    revert revert merch app

---
+++ chemie/merch/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class MerchCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Merch(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Navn")
+    price = models.FloatField(verbose_name="Pris")
+    image = models.ImageField(upload_to="merch", verbose_name="Bilde")
+    info = models.TextField(max_length=500, blank=True, verbose_name="Info")
+    category = models.ForeignKey(MerchCategory, verbose_name="Kategori", related_name="Category", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
---
+++ chemie/merch/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name="merch"
+
+urlpatterns = [
+    path("", views.all_merch, name="index"),
+    path("opprett/", views.create_merch, name="create"),
+    path("opprett/kategori", views.create_category, name="create_category"),
+    path("<int:pk>", views.detail, name="detail"),
+    path("delete/<int:pk>", views.delete, name="delete"),
+    path(
+        "category-autocomplete/",
+        views.CategoryAutocomplete.as_view(),
+        name="category-autocomplete",
+    ),
+]
