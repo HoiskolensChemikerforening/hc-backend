e39de4053612e8dcb84672e54b5ce730278847a6
Bendik Støa Sannes <bendik.sannes@gmail.com>
2023-03-01 21:12:25 +0100

    Sånn ferdig

--- chemie/chemie/settings/base.py
+++ chemie/chemie/settings/base.py
@@ -270,8 +270,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     "DEFAULT_PERMISSION_CLASSES": (
-        #"rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
-        #"rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.BasicAuthentication",
