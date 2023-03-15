510c9c8884033a38639d860cc9f474128f9a76c5
William <williamdavidsen@icloud.com>
2023-03-15 13:37:49 +0100

    Portofolio class

--- chemie/stock/models.py
+++ chemie/stock/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Stock(models.Model):
@@ -10,5 +10,13 @@ class History(models.Model):
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
     value = models.DecimalField(max_digits=None, decimal_places=2)
     stock = models.OneToOneField(Stock)
+
+class Porofolio(models.Model):
+    user = models.OneToOneField(User)
+    stock = models.OneToOneField(Stock)
+    volume = models.IntegerField(null=True)
+    balance = models.DecimalField(max_digits=None, decimal_places=2)
+
+
 
 
