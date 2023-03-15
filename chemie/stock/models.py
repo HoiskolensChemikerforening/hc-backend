ec092de1e1cf5fd029baf48601a5cb7743433866
ivanny <nygaardivan@gmail.com>
2023-03-15 13:21:17 +0100

    Added models and stuff

--- chemie/stock/models.py
+++ chemie/stock/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model):
+    name  = models.CharField(max_length=30)
+    desc  = models.TextField()
+
+class History(models.Model):
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    value = models.DecimalField(max_digits=None, decimal_places=2)
+    stock = models.OneToOneField(Stock)
+
+
