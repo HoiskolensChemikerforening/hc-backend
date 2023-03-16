7f97a8ead76fbb7aa452d7a34653445773a6b11d
Paul Groening <paul.jonathan.groening@gmail.com>
2023-03-16 15:57:11 +0100

    working version2

--- chemie/customprofile/migrations/0014_merge_20220907_1742.py
+++ chemie/customprofile/migrations/0014_merge_20220907_1742.py
@@ -6,8 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('customprofile', '0013_auto_20220324_1352'),
-        ('customprofile', '0013_auto_20220304_1513'),
     ]
 
     operations = [
