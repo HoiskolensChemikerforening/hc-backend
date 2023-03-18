c1850e9caaa0a4f1a22d208a51925471a5b523a1
ivanny <nygaardivan@gmail.com>
2023-03-18 11:53:44 +0100

    Fixed on_del

--- chemie/stock/models.py
+++ chemie/stock/models.py
@@ -8,14 +8,14 @@ class Stock(models.Model):
 
 class History(models.Model):
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
-    value = models.DecimalField(max_digits=None, decimal_places=2)
+    value = models.DecimalField(max_digits=12, decimal_places=2)
     stock = models.OneToOneField(Stock, on_delete=models.CASCADE)
 
 class Portfolio(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     stock = models.ManyToManyField(Stock)
     volume = models.IntegerField(null=True)
-    balance = models.DecimalField(max_digits=None, decimal_places=2)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
 
 
 
