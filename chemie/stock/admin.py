7f20254d874ffd712265c1219a7c1bd44a4feb8d
Dariela Ljubojevic <darielaljubojevic@dhcp-10-220-217-104.wlan.ntnu.no>
2023-03-18 13:50:50 +0100

    models final

--- chemie/stock/admin.py
+++ chemie/stock/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Stocktype, Stock, History, Portfolio
+
+admin.site.register(Stock)
+admin.site.register(Stocktype)
--- chemie/stock/models.py
+++ chemie/stock/models.py
@@ -2,20 +2,39 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Stock(models.Model):
+
+
+
+class Stocktype(models.Model):
     name  = models.CharField(max_length=30)
     desc  = models.TextField()
+
+    def create_stock(self, amount):
+        for i in range(amount):
+            stock = Stock()
+            stock.stocktype = self
+            stock.save()
+
+    def __str__(self):
+        return self.name #returnerer navnet på stocken istendenfor dritt
+
+
+class Portfolio(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    volume = models.IntegerField(null=True)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+
+class Stock(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.SET_NULL, null=True)
+    stocktype = models.ForeignKey(Stocktype, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.stocktype} Id:{self.id}"
 
 class History(models.Model):
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
     value = models.DecimalField(max_digits=12, decimal_places=2)
-    stock = models.OneToOneField(Stock, on_delete=models.CASCADE)
+    Stock = models.OneToOneField(Stock, on_delete=models.CASCADE)
 
-class Portfolio(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    stock = models.ManyToManyField(Stock)
-    volume = models.IntegerField(null=True)
-    balance = models.DecimalField(max_digits=12, decimal_places=2)
 
 
 
