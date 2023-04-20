623df2b686579b8d5995fcbf22617e6ed75e8a90
Paul Groening <paul.jonathan.groening@gmail.com>
2023-04-20 16:43:37 +0200

    kaare

--- chemie/cgp/tests/test_models.py
+++ chemie/cgp/tests/test_models.py
@@ -9,7 +9,7 @@ class CGPTestCase(TestCase):
         self.cgp = CGP.objects.create(year=1995)
         CGP.objects.create(year=2000)
         self.user = User.objects.create(
-            username="Kåre Oh", password="kann", email="gut@lesen.com"
+            username="Kaare Oh", password="kann", email="gut@lesen.com"
         )
 
     def test_if_get_latest_or_create_returns_latest(self):
