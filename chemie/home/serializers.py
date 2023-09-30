a20d8ea688d1bf844e7dc6076e276d123206c21d
William <williamdavidsen@icloud.com>
2023-09-30 16:38:19 +0200

    OfficeAccessSerializer bug fix

--- chemie/home/serializers.py
+++ chemie/home/serializers.py
@@ -15,8 +15,8 @@ class OfficeAccessSerializer(serializers.ModelSerializer):
     class Meta:
         model = OfficeApplication
         fields = "__all__"
-    def get_alreadyApplied(self, test):
-        author = self.request.user  # hente student username fra form
+    def get_alreadyApplied(self, req):
+        author = req.author  # hente student username fra form
         already_applied = OfficeApplication.objects.filter(
         author=author, created__gte=timezone.now() - timezone.timedelta(days=30),).exists()
         return already_applied
