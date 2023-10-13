2be48598fec0892179eecad22deca19c1c6a9729
Paul Groening <paul.jonathan.groening@gmail.com>
2023-10-13 23:54:57 +0200

    preselect

--- chemie/electofood/views.py
+++ chemie/electofood/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import ElectionQuestionForm, CommiteeAnswer, VALUES
+from .models import ElectionQuestionForm, CommiteeAnswer, VALUES,UserAnswer
 from chemie.committees.models import Committee
 from django.contrib.auth.decorators import login_required
 from .forms import AnswerForm
 
+
+from django.template.defaulttags import register
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
 @login_required
 def index(request):
@@ -27,7 +33,17 @@ def index(request):
 def valgomat_form(request, id):
     electionform = get_object_or_404(ElectionQuestionForm, id=id)
     questions = electionform.electionquestion_set.all()
-    form = AnswerForm(request.POST or None)
+
+    #print([question.answer_set.filter(useranswer__user=request.user) for question in questions])
+    user_answers = UserAnswer.objects.filter(user=request.user).filter(question__question_form=electionform)
+    answer_dict = None
+    if len(user_answers) == len(questions):
+        answer_dict = {}
+        for user_answer in user_answers:
+            answer_dict[user_answer.question.id]=user_answer.answer
+
+
+
     if request.POST:
         valid = True
         for question in questions:
@@ -40,10 +56,10 @@ def valgomat_form(request, id):
 
 
 
-
+    print(answer_dict)
     context = {
         "questions": questions,
         "values": VALUES,
-        "form": form
+        "answer_dict":answer_dict
     }
     return render(request, "electofeedform.html", context)
