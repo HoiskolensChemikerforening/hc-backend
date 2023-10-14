104a98df04386faa6a33caa17fc101c39c8e27dd
Paul Groening <paul.jonathan.groening@gmail.com>
2023-10-14 08:57:21 +0200

    add commitee answer possibility

--- chemie/electofood/views.py
+++ chemie/electofood/views.py
@@ -5,9 +5,12 @@ from chemie.committees.models import Committee
 from django.contrib.auth.decorators import login_required
 from .forms import AnswerForm
 from django.urls import reverse
+import random
 
 
 from django.template.defaulttags import register
+
+COLORS = ["#ea5545", "#f46a9b", "#ef9b20", "#edbf33", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6"]
 
 @register.filter
 def get_item(dictionary, key):
@@ -31,18 +34,21 @@ def index(request):
 
 
 @login_required()
-def valgomat_form(request, id):
+def valgomat_form(request, id, committe_id=None):
     electionform = get_object_or_404(ElectionQuestionForm, id=id)
     questions = electionform.electionquestion_set.all()
 
-    user_answers = UserAnswer.objects.filter(user=request.user).filter(question__question_form=electionform)
+    if not committe_id:
+        answers = UserAnswer.objects.filter(user=request.user).filter(question__question_form=electionform)
+        committee = None
+    else:
+        answers = CommiteeAnswer.objects.filter(committee__id=committe_id).filter(question__question_form=electionform)
+        committee = get_object_or_404(Committee, id=committe_id)
     answer_dict = None
-    if len(user_answers) == len(questions):
+    if len(answers) == len(questions):
         answer_dict = {}
-        for user_answer in user_answers:
-            answer_dict[user_answer.question.id]=user_answer.answer
-
-
+        for answer in answers:
+            answer_dict[answer.question.id] = answer.answer
 
     if request.POST:
         valid = True
@@ -52,17 +58,21 @@ def valgomat_form(request, id):
         if valid:
             if not answer_dict:
                 for question in questions:
-                    user_answer = UserAnswer()
-                    user_answer.user = request.user
-                    user_answer.question = question
-                    user_answer.answer = int(request.POST[str(question.id)])
-                    user_answer.save()
-                return redirect(reverse("valgomat:valgomat_result", kwargs={"id":id}))
+                    if not committe_id:
+                        answer = UserAnswer()
+                        answer.user = request.user
+                    else:
+                        answer = CommiteeAnswer()
+                        answer.committee = committee
+                    answer.question = question
+                    answer.answer = int(request.POST[str(question.id)])
+                    answer.save()
+                return redirect(reverse("valgomat:valgomat_result", kwargs={"id": id}))
             else:
-                for user_answer in user_answers:
-                    user_answer.answer = int(request.POST[str(user_answer.question.id)])
-                    user_answer.save()
-                return redirect(reverse("valgomat:valgomat_result", kwargs={"id":id}))
+                for answer in answers:
+                    answer.answer = int(request.POST[str(answer.question.id)])
+                    answer.save()
+                return redirect(reverse("valgomat:valgomat_result", kwargs={"id": id}))
 
 
 
@@ -71,7 +81,7 @@ def valgomat_form(request, id):
     context = {
         "questions": questions,
         "values": VALUES,
-        "answer_dict":answer_dict
+        "answer_dict": answer_dict
     }
     return render(request, "electofeedform.html", context)
 
@@ -81,11 +91,12 @@ def valgomat_result(request, id):
     electionform = get_object_or_404(ElectionQuestionForm, id=id)
     committees = electionform.get_participating_committes()
     results = []
-    colors = ["#ea5545", "#f46a9b", "#ef9b20", "#edbf33", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6"]
+    colors = COLORS
+    random.shuffle(colors)
     for i, committe in enumerate(committees):
         results.append([committe, electionform.calculate_result_commitee(request.user, committe),colors[(i%len(colors))]])
     results.sort(key=lambda x: x[1], reverse=True)
     context={
-        "results":results,
+        "results": results,
     }
     return render(request, "electofeedresults.html", context)
