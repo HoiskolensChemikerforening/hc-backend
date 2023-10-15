29dffae4765e80ea614053b54cec99f6984ba797
Paul Groening <paul.jonathan.groening@gmail.com>
2023-10-15 18:12:10 +0200

    admin comments

--- chemie/electofood/admin.py
+++ chemie/electofood/admin.py
@@ -6,20 +6,21 @@ from .models import (
     UserAnswer,
 )
 
+# Registrer ElectionQuestionForm
 admin.site.register(ElectionQuestionForm)
 
-
+# Registrer ElectionQuestion
 @admin.register(ElectionQuestion)
 class ElectionQuestionAdmin(admin.ModelAdmin):
     list_display = ["question", "question_form"]
 
-
+# Registrer CommiteeAnswer
 @admin.register(CommiteeAnswer)
 class CommiteeAnswerAdmin(admin.ModelAdmin):
     list_display = ["committee", "question", "answer", "get_election_form"]
 
 
-# admin.site.register(CommiteeAnswer)
+# Registrer UserAnswer
 @admin.register(UserAnswer)
 class UserAnswerAdmin(admin.ModelAdmin):
     list_display = ["user", "question", "answer", "get_election_form"]
