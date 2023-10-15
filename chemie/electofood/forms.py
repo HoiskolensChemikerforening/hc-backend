748b4c86e23a8699d244797b8b440ed8a5c9e209
Paul Groening <paul.jonathan.groening@gmail.com>
2023-10-15 18:13:27 +0200

    form comments

--- chemie/electofood/forms.py
+++ chemie/electofood/forms.py
@@ -3,12 +3,18 @@ from .models import ElectionQuestionForm, ElectionQuestion
 
 
 class ElectionQuestionFormForm(forms.ModelForm):
+    """
+    Form to create new ElectionQuestionForm objects.
+    """
     class Meta:
         model = ElectionQuestionForm
         fields = ["title", "description"]
 
 
 class ElectionQuestionCreateForm(forms.ModelForm):
+    """
+    Form to create new ElectionQuestion objects.
+    """
     class Meta:
         model = ElectionQuestion
         fields = ["question"]
