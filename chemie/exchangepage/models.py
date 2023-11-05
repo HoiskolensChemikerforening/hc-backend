eec2fda75c946ebb83972778462e6bbe0798ffc0
Ivan Nygaard <ivan@dhcp-10-24-104-210.wlan.ntnu.no>
2023-11-05 13:57:51 +0100

    modelschange

--- chemie/exchangepage/models.py
+++ chemie/exchangepage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from chemie.customprofile.models import Profile
+class Travelletter(models.Model):
+    user        = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    country     = models.CharField(max_length=30)
+    city        = models.CharField(max_length=30)
+    std1        = models.IntegerField(default=0, verbose_name="Score, std1")
+    std2        = models.IntegerField(default=0, verbose_name="Score, std2")
+    std3        = models.IntegerField(default=0, verbose_name="Score, std3")
+    std4        = models.IntegerField(default=0, verbose_name="Score, std4")
+    std5        = models.IntegerField(default=0, verbose_name="Score, std5")
+    std6        = models.IntegerField(default=0, verbose_name="Score, std6")
+    def __str__(self):
+        return self.title
 
-# Create your models here.
+
+
+
+
