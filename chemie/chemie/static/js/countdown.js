ba00fdf0f1d7a2de583c14c5f698329c3f0f517c
taraldkb <bishoptarald@gmail.com>
2023-12-26 18:17:18 +0100

    Make js function more general and add hour and minute functionality

--- chemie/chemie/static/js/countdown.js
+++ chemie/chemie/static/js/countdown.js
@@ -1,14 +1,29 @@
 dayjs.extend(dayjs_plugin_duration);
 
-function dayCountdown(element,targetString){
+function dayCountdown(element,targetString,eventString){
     const targetDate = dayjs(targetString);
 
     setInterval(()=>{
         const now = dayjs();
         const duration = dayjs.duration(targetDate.diff(now));
 
-        if (duration.asMillisecond <= 0) return;
-        element.querySelector(".days__until").textContent = duration.asDays().toFixed(0).toString().padStart(2,"0")
+        if (duration.asMilliseconds() <= 0) {
+            element.querySelector(".until__text").textContent = ` ${eventString}!!`
+            return
+        };
+
+        if (duration.asDays()>=1){
+            element.querySelector(".days__until").textContent = duration.asDays().toFixed(0).toString().padStart(2,"0")
+            element.querySelector(".until__text").textContent = `Dager igjen til ${eventString}`
+        };
+        if (duration.asDays()<1){
+            element.querySelector(".days__until").textContent = duration.asHours().toFixed(0).toString().padStart(2,"0")
+            element.querySelector(".until__text").textContent = `Timer igjen til ${eventString}`
+        };
+        if (duration.asHours()<1){
+            element.querySelector(".days__until").textContent = duration.asMinutes().toFixed(0).toString().padStart(2,"0")
+            element.querySelector(".until__text").textContent = `Minutter igjen til ${eventString} `
+        };
 
     },50)
 
@@ -16,4 +31,4 @@ function dayCountdown(element,targetString){
 
 }
 
-dayCountdown(document.getElementById("Countdown"),"2024-03-04")
+dayCountdown(document.getElementById("Countdown"),"2023-03-04 12:00","vinterblotet")
