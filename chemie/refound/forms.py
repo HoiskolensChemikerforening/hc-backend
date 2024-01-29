2b52f9ec94c59bcc523ca8c363e3adaa48896a5f
Paul Groening <paul.jonathan.groening@gmail.com>
2024-01-29 21:44:16 +0100

    comments form

--- chemie/refound/forms.py
+++ chemie/refound/forms.py
@@ -5,11 +5,16 @@ from django.core.validators import MinLengthValidator
 
 
 class DatePickerInput(forms.DateTimeInput):
+    """
+    Class to add a date picker to the date field.
+    """
     input_type = "date"
 
 
 class AccountNumberForm(forms.ModelForm):
-    # account_number = forms.CharField(max_length=11, validators=[MinLengthValidator(11)])
+    """
+    Form to create a RefoundRequest object.
+    """
     class Meta:
         model = RefoundRequest
         fields = ["account_number"]
@@ -21,16 +26,22 @@ class AccountNumberForm(forms.ModelForm):
 
 
 class RefoundForm(forms.ModelForm):
+    """
+    Form to create a Refound object.
+    """
     class Meta:
         model = Refound
         fields = ["date", "store", "item", "event", "price", "image"]
         widgets = {"date": DatePickerInput()}
 
     def __init__(self, *arg, **kwarg):
+        """
+        Override __init__ to ensure that the form must contain data and can not be submitted empty.
+        """
         super(RefoundForm, self).__init__(*arg, **kwarg)
         self.empty_permitted = False
 
-
+# Create a formset for the refound object to enable multible refound forms on one page.
 RefoundFormSet = modelformset_factory(
     Refound,
     form=RefoundForm,
