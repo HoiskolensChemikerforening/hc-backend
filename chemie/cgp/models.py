20327ae013ed76eaca35ddbccc4a2d73b1ec1967
Paul Groening <paul.jonathan.groening@gmail.com>
2024-02-25 14:58:55 +0100

    fix add country after votes have been submittet crash

--- chemie/cgp/models.py
+++ chemie/cgp/models.py
@@ -321,12 +321,19 @@ class Vote(models.Model):
             .split(",")
         )
         vote_dict = {vote_list[i]: i for i in range(len(vote_list))}
-        group_list = sorted(
-            list(
-                self.group.cgp.group_set.exclude(id=self.group.id).exclude(
+        voteable_groups = self.group.cgp.group_set.exclude(id=self.group.id).exclude(
                     audience=True
                 )
+        # Place newly created groups not contained in the previous vote groups last
+        for group in voteable_groups:
+            if group.country.country_name not in vote_dict.keys():
+                vote_dict[group.country.country_name] = len(vote_list)
+
+        group_list = sorted(
+            list(
+                voteable_groups
             ),
             key=lambda group: vote_dict[group.country.country_name],
         )
+
         return group_list, self.failureprize_vote, self.showprize_vote
