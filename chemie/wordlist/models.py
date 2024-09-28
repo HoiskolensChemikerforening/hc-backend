9ed105c592ac665e42addf76da14196138361def
Fredrik Stenberg <freandste01@outlook.com>
2024-09-28 15:16:16 +0200

    fix2

--- chemie/wordlist/models.py
+++ chemie/wordlist/models.py
@@ -18,12 +18,10 @@ class AbstractWord(models.Model):
     secret = models.BooleanField(default=False, verbose_name="Hemmelig?")
     category = models.ManyToManyField(Category, verbose_name="Kategori", blank=True)
     
-    def __str__(self):
-        return self.word
     
 
 class Noun(AbstractWord):
-    word = models.CharField(max_length = 100, verbose_name = "ubestemt_entall", unique = False)
+    noun = models.CharField(max_length = 100, verbose_name = "ubestemt_entall", unique = False)
     indefinite_plural = models.CharField(max_length = 100, verbose_name = "ubestemt_flertall", unique = False)
     definite_singular = models.CharField(max_length = 100, verbose_name = "bestemt_entall", unique = False)
     definite_plural = models.CharField(max_length = 100, verbose_name = "bestemt_flertall", unique = False)
@@ -32,7 +30,7 @@ class Noun(AbstractWord):
          return self.word
 
 class Verb(AbstractWord):
-    word = models.CharField(max_length = 100, verbose_name = "infinitiv", unique = False)
+    verb = models.CharField(max_length = 100, verbose_name = "infinitiv", unique = False)
     present = models.CharField(max_length = 100, verbose_name = "presens", unique = False)
     past = models.CharField(max_length = 100, verbose_name = "preteritum", unique = False)
     future = models.CharField(max_length = 100, verbose_name = "presens futurum", unique = False)
