9003d2becb5ddcec06255b55ab78347e0c4ff0ae
Paul Groening <paul.jonathan.groening@gmail.com>
2025-02-13 10:01:51 +0100

    svin og poels

---
+++ chemie/villsvin/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Villsvin(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Korv(models.Model):
+    weight = models.DecimalField(decimal_places=2, max_digits=8)
+    length = models.DecimalField(decimal_places=2, max_digits=8)
+    name = models.CharField(max_length=30)
+    boar = models.ForeignKey(Villsvin, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} {self.weight}"
--- chemie/villsvin/views.py
+++ chemie/villsvin/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from .models import Villsvin
 
 def index(request):
-
+    a = Villsvin.objects.all()
     context = {}
 
     return render(request, "index.html", context)
