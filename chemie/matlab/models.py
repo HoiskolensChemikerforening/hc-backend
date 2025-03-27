398faf7f6052859a6b70974ed87b4a7871d7741e
Andreas Skaane <229andreas@gmail.com>
2025-03-27 08:46:26 +0100

    Added categories to Recipies

--- chemie/matlab/models.py
+++ chemie/matlab/models.py
@@ -3,23 +3,29 @@ from django.db import models
 
 class Ingredients(models.Model):
     name = models.CharField(max_length=100)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField() #1kg løk og 1stk løk vil være to ingredienser... løk er jo løk
     unit = models.CharField(max_length=100)
 
     
     def __str__(self):
         return f"{self.name}"
 
+class Kategori(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class Recipes(models.Model):
     name = models.CharField(max_length=30)
     price = models.PositiveSmallIntegerField()
     time = models.PositiveSmallIntegerField()
     ingredients = models.ManyToManyField(Ingredients, blank = True)
+    categories = models.ManyToManyField(Kategori, blank=True)
 
     def __str__(self):
         return f"{self.name}"
-   
+
 
 #class Korv(models.Model):
 #    weight = models.DecimalField(decimal_places=2, max_digits=8)
