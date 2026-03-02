e0fc3572dd757f0b2a3bc10595fd0b8059c5ad7c
Magnhild Nordgård <magnhild.nordgard@gmail.com>
2026-03-02 15:47:28 +0100

    small fix

--- chemie/test_app_2/models.py
+++ chemie/test_app_2/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
+COLORS = {
+        "Red","Blue","Brown","Black"
+    }
 class Book(models.Model):
     title = models.CharField(max_length=30)
     authour = models.CharField(max_length=30)
-    COLORS = {
-        "Red","Blue","Brown","Black"
-    }
     color = models.CharField(max_length=1,choices=COLORS)
 
 class Publisher(models.Model):
