7cecf6347f6da919a687fccf325de01699ddff52
Magnhild Nordgård <magnhild.nordgard@gmail.com>
2026-03-02 14:54:19 +0100

    added bookclass

--- chemie/test_app_2/models.py
+++ chemie/test_app_2/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class Book(models.Model):
-    
+    title = models.CharField(max_length=30)
+    authour = models.CharField(max_length=30)
+    COLORS = {
+        "Red","Blue","Brown","Black"
+    }
+    color = models.CharField(max_length=1,choices=COLORS)
+
+class 
