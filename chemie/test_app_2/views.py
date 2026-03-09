445576cde836b3e649a74d93af057367ed4681af
Hannah Angela Ekblad <hannaae@stud.ntnu.no>
2026-03-09 14:57:03 +0100

    endringer

--- chemie/test_app_2/views.py
+++ chemie/test_app_2/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
+from .models import Book
+from .forms import BookForm
 
 
 def index(request):
@@ -12,12 +15,42 @@ def index(request):
     return render(request, "krokodille.html", context)
 
 
-def index_2(request):
-    context = {}
+def index_2(request): # http://127.0.0.1:8000/test_app_2/krokodille_2/
+
+    all_books = Book.objects.all()
+
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                f"Nytt spørsmål opprettet!",
+                extra_tags="Suksess",
+            )
+            # Redirect to the desired page after successful editing
+            return redirect("test_app_2:index_2")
+
+
+    else:
+        form = BookForm()
+
+    context = {"html_form":form, "html_books":all_books}
+
 
     return render(request, "krokodille2.html", context)
+
+
+
+
+
 
 def dih(request):
     context = {}
     #return HttpResponseRedirect(reverse("test_app:jeg_er_fra_test_app"))
-    return render(request, "test_app:jeg_er_fra_Test_app.html", context)
+    # return render(request, "test_app:jeg_er_fra_Test_app.html", context)
+    print("dih kjører")
+    return redirect("test_app:index")
+
