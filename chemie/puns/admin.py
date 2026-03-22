2e59025e36efa711988edb74931377528f80c1e6
Hannah Angela Ekblad <hannaae@stud.ntnu.no>
2026-03-22 15:39:10 +0100

    Alt er endra yay

--- chemie/puns/admin.py
+++ chemie/puns/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from .models import Submission
+from .models import Puns_Submission
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["content", "author", "date", "used"]
+    list_display = ["content", "author", "date", "accepted"]
     list_filter = ["date"]
     search_fields = ["content", "author__username"]
     list_display_links = None
 
     class Meta:
-        model = Submission
+        model = Puns_Submission
 
 
-admin.site.register(Submission, PostAdmin)
+admin.site.register(Puns_Submission, PostAdmin)
--- chemie/puns/models.py
+++ chemie/puns/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Submission(models.Model):
+class Puns_Submission(models.Model):
     content = models.TextField(max_length=3000, verbose_name="Vits")
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User, verbose_name="Innsender", on_delete=models.CASCADE
     )
-    used = models.BooleanField(
+    accepted = models.BooleanField(
         default=False
     )  # For Webkom to keep track on used submissions
